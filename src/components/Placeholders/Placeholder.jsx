266cb7eb9b92c44dd58b18df27e0961103c304aa
agirard <merkkur39@gmail.com>
2021-07-08 16:10:42 +0200

    feat: Add ImportDropdown component

--- src/components/Placeholders/Placeholder.jsx
+++ src/components/Placeholders/Placeholder.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import ActionMenu from 'cozy-ui/transpiled/react/ActionMenu'
 import ListItem from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItem'
 import ListItemIcon from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItemIcon'
 import Divider from 'cozy-ui/transpiled/react/MuiCozyTheme/Divider'
@@ -8,17 +9,22 @@ import IconStack from 'cozy-ui/transpiled/react/IconStack'
 import InfosBadge from 'cozy-ui/transpiled/react/InfosBadge'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import FileDuotoneIcon from 'cozy-ui/transpiled/react/Icons/FileDuotone'
-import People from 'cozy-ui/transpiled/react/Icons/People'
 import Plus from 'cozy-ui/transpiled/react/Icons/Plus'
 import { useI18n } from 'cozy-ui/react/I18n'
 import { getCssVariableValue } from 'cozy-ui/transpiled/react/utils/color'
 
+import ImportDropdown from '../ImportDropdown'
+
 const Placeholder = ({ placeholder, divider }) => {
   const { t } = useI18n()
+  const [isDrawerDisplayed, setIsDrawerDisplayed] = useState(false)
+
+  const showDrawer = () => setIsDrawerDisplayed(true)
+  const hideDrawer = () => setIsDrawerDisplayed(false)
 
   return (
     <>
-      <ListItem key={placeholder.label}>
+      <ListItem key={placeholder.label} onClick={showDrawer}>
         <ListItemIcon>
           <InfosBadge
             badgeContent={
@@ -37,10 +43,9 @@ const Placeholder = ({ placeholder, divider }) => {
                   size={32}
                 />
               }
-              // FIX Import dynamic Icon according to "placeholder.label"
               foreground={
                 <Icon
-                  icon={People}
+                  icon={placeholder.icon}
                   color={getCssVariableValue('dodgerBlue')}
                   size={16}
                 />
@@ -53,6 +58,12 @@ const Placeholder = ({ placeholder, divider }) => {
         </Typography>
       </ListItem>
       {divider && <Divider variant="inset" component="li" />}
+
+      {isDrawerDisplayed && (
+        <ActionMenu onClose={hideDrawer}>
+          <ImportDropdown label={placeholder.label} icon={placeholder.icon} />
+        </ActionMenu>
+      )}
     </>
   )
 }
--- test/components/AppLike.jsx
+++ test/components/AppLike.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { CozyProvider, createMockClient } from 'cozy-client'
 import I18n from 'cozy-ui/react/I18n'
-import { BreakpointsProvider } from 'cozy-ui/react/hooks/useBreakpoints'
+import { BreakpointsProvider } from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 import { HashRouter } from 'react-router-dom'
 import enLocale from '../../src/locales/en.json'
 
