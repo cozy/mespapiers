9bf322e24cba5adf5faafd80e209b68f54ae588d
Alexis G <merkkur39@gmail.com>
2021-07-19 10:04:31 +0200

    feat: Add DialogInStepper component

---
+++ src/components/DialogInStepper/DialogInStepper.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import cx from 'classnames'
+
+import {
+  useCozyDialog,
+  DialogBackButton,
+  DialogCloseButton
+} from 'cozy-ui/transpiled/react/CozyDialogs'
+import MUIDialog, {
+  DialogTitle,
+  DialogContent
+} from 'cozy-ui/transpiled/react/Dialog'
+import Divider from 'cozy-ui/transpiled/react/MuiCozyTheme/Divider'
+
+const DialogInStepper = props => {
+  const { onClose, title, content } = props
+  const { dialogProps, dialogTitleProps, fullScreen, id } = useCozyDialog(props)
+
+  return (
+    <MUIDialog {...dialogProps}>
+      {!fullScreen && onClose && (
+        <DialogCloseButton
+          onClick={onClose}
+          data-test-id={`modal-close-button-${id}`}
+        />
+      )}
+      <DialogTitle
+        {...dialogTitleProps}
+        className={cx('u-ellipsis', { dialogTitleFull: !onClose })}
+      >
+        {fullScreen && onClose && <DialogBackButton onClick={onClose} />}
+        {title}
+      </DialogTitle>
+      <Divider />
+      <DialogContent classes={{ root: 'actionsInContent' }}>
+        {content}
+      </DialogContent>
+    </MUIDialog>
+  )
+}
+
+DialogInStepper.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func,
+  title: PropTypes.node,
+  content: PropTypes.node,
+  size: PropTypes.oneOf(['small', 'medium', 'large'])
+}
+
+export default DialogInStepper
