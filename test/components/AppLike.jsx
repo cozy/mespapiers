7a321ba1cb2554302d513104abfc49ee96214b60
Alexis G <merkkur39@gmail.com>
2021-07-20 12:12:24 +0200

    feat: Add StepperProvider

--- test/components/AppLike.jsx
+++ test/components/AppLike.jsx
@@ -4,15 +4,20 @@ import I18n from 'cozy-ui/transpiled/react/I18n'
 import { BreakpointsProvider } from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 import { HashRouter } from 'react-router-dom'
 import enLocale from '../../src/locales/en.json'
-import { DialogModalProvider } from '../../src/components/Contexts'
+import {
+  DialogModalProvider,
+  StepperProvider
+} from '../../src/components/Contexts'
 
 const AppLike = ({ children, client }) => (
   <CozyProvider client={client || createMockClient({})}>
     <I18n dictRequire={() => enLocale} lang={'en'}>
       <BreakpointsProvider>
-        <DialogModalProvider>
-          <HashRouter>{children}</HashRouter>
-        </DialogModalProvider>
+        <StepperProvider>
+          <DialogModalProvider>
+            <HashRouter>{children}</HashRouter>
+          </DialogModalProvider>
+        </StepperProvider>
       </BreakpointsProvider>
     </I18n>
   </CozyProvider>
