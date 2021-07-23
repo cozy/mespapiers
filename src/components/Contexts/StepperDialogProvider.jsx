66fd944719a86fb77e1d660d8ff3210f2c5aa62d
Alexis G <merkkur39@gmail.com>
2021-07-23 16:03:37 +0200

    feat: Update papers definitions

--- src/components/Contexts/StepperDialogProvider.jsx
+++ src/components/Contexts/StepperDialogProvider.jsx
@@ -1,30 +1,43 @@
-import React, { createContext, useCallback, useState } from 'react'
+import React, { createContext, useCallback, useEffect, useState } from 'react'
 
 const StepperDialogContext = createContext()
 
 const StepperDialogProvider = ({ children }) => {
   const [isStepperDialogOpen, setIsStepperDialogOpen] = useState(false)
   const [stepperDialogTitle, setStepperDialogTitle] = useState('')
+  const [allCurrentPagesDefinitions, setAllCurrentPagesDefinitions] = useState(
+    []
+  )
   const [allCurrentPages, setAllCurrentPages] = useState([])
-  const [currentPage, setCurrentPage] = useState(1)
+  const [currentPageIndex, setCurrentPageIndex] = useState(1)
+
+  useEffect(() => {
+    if (allCurrentPagesDefinitions.length > 0) {
+      const allCurrentPagesDefinitionsSorted = allCurrentPagesDefinitions.sort(
+        (a, b) => a.pageIndex - b.pageIndex
+      )
+      setAllCurrentPages(allCurrentPagesDefinitionsSorted)
+    }
+  }, [allCurrentPagesDefinitions])
 
   const previousPage = useCallback(() => {
-    currentPage > 1
-      ? setCurrentPage(prev => prev - 1)
+    currentPageIndex > 1
+      ? setCurrentPageIndex(prev => prev - 1)
       : setIsStepperDialogOpen(false)
-  }, [currentPage])
+  }, [currentPageIndex])
 
   const nextPage = useCallback(() => {
-    allCurrentPages.length > currentPage && setCurrentPage(prev => prev + 1)
-  }, [currentPage, allCurrentPages.length])
+    allCurrentPages.length > currentPageIndex &&
+      setCurrentPageIndex(prev => prev + 1)
+  }, [allCurrentPages.length, currentPageIndex])
 
   const stepperDialog = {
     isStepperDialogOpen,
     allCurrentPages,
-    currentPage,
+    currentPageIndex,
     stepperDialogTitle,
     setIsStepperDialogOpen,
-    setAllCurrentPages,
+    setAllCurrentPagesDefinitions,
     setStepperDialogTitle,
     previousPage,
     nextPage
