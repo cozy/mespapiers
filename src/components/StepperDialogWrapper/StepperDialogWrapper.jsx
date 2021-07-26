25335fa9cefb73bd9cd92e662b83ed3098513be0
Alexis G <merkkur39@gmail.com>
2021-07-26 17:38:47 +0200

    feat: Add stepper info to StepperDialog

--- src/components/StepperDialogWrapper/StepperDialogWrapper.jsx
+++ src/components/StepperDialogWrapper/StepperDialogWrapper.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 
-import { useStepperDialogContext } from 'components/Hooks'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+
+import { useStepperDialogContext } from '../Hooks'
+import { StepperDialog } from '../StepperDialog'
 
 const StepperDialogWrapper = () => {
-  const { allCurrentPages, currentPageIndex } = useStepperDialogContext()
+  const { t } = useI18n()
+  const {
+    allCurrentPages,
+    currentPage,
+    stepperDialogTitle,
+    previousPage
+  } = useStepperDialogContext()
 
   return allCurrentPages.map(
     page =>
-      page.pageIndex === currentPageIndex && (
-        <React.Fragment key={page.pageIndex}></React.Fragment>
+      page.pageNumber === currentPage && (
+        <StepperDialog
+          key={page.pageNumber}
+          open
+          onClose={previousPage}
+          title={t(`items.${stepperDialogTitle}`)}
+          // TODO content={<LazyloadComponent />}
+          stepper={`${currentPage}/${allCurrentPages.length}`}
+        />
       )
   )
 }
