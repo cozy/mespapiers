e0ab262d82d8299abe9d46c26aea5e4dbb4b98d6
Alexis G <merkkur39@gmail.com>
2021-08-11 17:39:15 +0200

    feat: Add tests for "useQuery" hook

---
+++ src/components/Hooks/useQuery.spec.jsx
@@ -0,0 +1,42 @@
+import { useQuery as useQueryCozy } from 'cozy-client'
+import { useQuery } from 'src/components/Hooks/useQuery'
+
+jest.mock('cozy-client/dist/hooks/useQuery', () => jest.fn())
+
+describe('useQuery', () => {
+  it('should return correct object when data is "pending"', () => {
+    useQueryCozy.mockReturnValueOnce({
+      fetchStatus: 'pending',
+      data: null
+    })
+    const result = useQuery({ definition: '', options: '' })
+    expect(result).toEqual({
+      isQueryLoading: true,
+      allPapers: []
+    })
+  })
+
+  it('should return correct object when data is "loading"', () => {
+    useQueryCozy.mockReturnValueOnce({
+      fetchStatus: 'loading',
+      data: []
+    })
+    const result = useQuery({ definition: '', options: '' })
+    expect(result).toEqual({
+      isQueryLoading: true,
+      allPapers: []
+    })
+  })
+
+  it('should return correct object when data is "loaded"', () => {
+    useQueryCozy.mockReturnValueOnce({
+      fetchStatus: 'loaded',
+      data: [{ id: '00', name: 'ID card' }, { id: '01', name: 'Passport' }]
+    })
+    const result = useQuery({ definition: '', options: '' })
+    expect(result).toEqual({
+      isQueryLoading: false,
+      allPapers: [{ id: '00', name: 'ID card' }, { id: '01', name: 'Passport' }]
+    })
+  })
+})
