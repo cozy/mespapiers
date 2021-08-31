2cb3a3015f8ebc07f1fadcefdd37d37d1c4acca8
Alexis G <merkkur39@gmail.com>
2021-08-31 13:32:55 +0200

    feat: Disable redirection to konnectors

--- src/components/ImportDropdown/ImportDropdown.jsx
+++ src/components/ImportDropdown/ImportDropdown.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
-import { useClient } from 'cozy-client'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import List from 'cozy-ui/transpiled/react/MuiCozyTheme/List'
 import ListItem from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItem'
@@ -15,20 +14,14 @@ import Camera from 'cozy-ui/transpiled/react/Icons/Camera'
 
 import { useStepperDialogContext } from 'src/components/Hooks/useStepperDialogContext'
 import { useScannerI18n } from 'src/components/Hooks/useScannerI18n'
-import { getFilteredStoreUrl } from 'src/utils/getFilteredStoreUrl'
 import Konnector from 'src/assets/icons/Konnectors.svg'
 
 const ImportDropdown = ({ label, icon }) => {
   const { t } = useI18n()
   const scannerT = useScannerI18n()
-  const client = useClient()
   const [showModal, setShowModal] = useState(false)
 
   const { setIsStepperDialogOpen } = useStepperDialogContext()
-
-  const goToStore = () => {
-    window.location = getFilteredStoreUrl(client)
-  }
 
   // Avoid a potential memory leak.
   // When calling "setIsStepperDialogOpen" to "true", "Home" is unmounted to be replaced by the "Stepper".
@@ -61,7 +54,7 @@ const ImportDropdown = ({ label, icon }) => {
         </ListItem>
       </List>
       <List>
-        <ListItem onClick={goToStore}>
+        <ListItem disabled>
           <ListItemIcon>
             <Icon icon={Konnector} size={24} />
           </ListItemIcon>
