093be1b998d3f8d8095d3167d2efcfb3ba095bad
Alexis G <merkkur39@gmail.com>
2021-10-05 17:41:24 +0200

    feat: Add informations to Qualification panel

--- src/components/Viewer/Panel/Qualification.jsx
+++ src/components/Viewer/Panel/Qualification.jsx
@@ -1,6 +1,7 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
+import { getBoundT } from 'cozy-scanner'
+import { useClient } from 'cozy-client'
 import List from 'cozy-ui/transpiled/react/MuiCozyTheme/List'
 import ListItem from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItem'
 import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
@@ -12,26 +13,53 @@ import en from '../locales/fr.json'
 
 const locales = { fr, en }
 
-const Qualification = ({ file, t }) => {
-  const { name, metadata } = file || {}
-  const qualificationLabel = metadata?.qualification.label
+const validPageLabel = page => page === 'front' || page === 'back'
+
+const Qualification = ({ file, t, f, lang }) => {
+  const scannerT = getBoundT(lang)
+  const client = useClient()
+  const [currentUser, setCurrentUser] = useState(null)
+
+  const { name: filename, metadata } = file || {}
+  const { qualification } = metadata || {}
+  const { label, page: pageLabel, featureDate } = qualification || {}
+
+  // TODO Improve it when other contact choices are needed
+  useEffect(() => {
+    let isMounted = true
+    ;(async () => {
+      const contactCollection = client.collection('io.cozy.contacts')
+      const { data: currentUser } = await contactCollection.findMyself()
+      isMounted && setCurrentUser(currentUser[0])
+    })()
+
+    return () => {
+      isMounted = false
+    }
+  }, [client])
 
   return (
     <List className={'u-pv-half'}>
       <ListItem>
         <Typography variant={'h6'}>{t('viewer.panel.title')}</Typography>
       </ListItem>
-      <ListItem>
-        <ListItemText
-          disableTypography
-          primary={
-            <Typography variant={'caption'}>
-              {t('viewer.panel.qualification.date')}
-            </Typography>
-          }
-          secondary={<Typography variant={'body1'}>In coming</Typography>}
-        />
-      </ListItem>
+      {featureDate && (
+        <ListItem>
+          <ListItemText
+            disableTypography
+            primary={
+              <Typography variant={'caption'}>
+                {t(`viewer.panel.qualification.date.title.${featureDate}`)}
+              </Typography>
+            }
+            secondary={
+              <Typography variant={'body1'}>
+                {f(qualification[featureDate], 'DD/MM/YYYY')}
+              </Typography>
+            }
+          />
+        </ListItem>
+      )}
       <ListItem>
         <ListItemText
           disableTypography
@@ -40,7 +68,9 @@ const Qualification = ({ file, t }) => {
               {t('viewer.panel.qualification.identity')}
             </Typography>
           }
-          secondary={<Typography variant={'body1'}>In coming</Typography>}
+          secondary={
+            <Typography variant={'body1'}>{currentUser?.fullname}</Typography>
+          }
         />
       </ListItem>
       <ListItem>
@@ -48,10 +78,16 @@ const Qualification = ({ file, t }) => {
           disableTypography
           primary={
             <Typography variant={'caption'}>
-              {t('viewer.panel.qualification.label')}
+              {t('viewer.panel.qualification.label.title')}
             </Typography>
           }
-          secondary={<Typography variant={'body1'}>{name}</Typography>}
+          secondary={
+            <Typography variant={'body1'}>
+              {validPageLabel(pageLabel)
+                ? t(`viewer.panel.qualification.label.${pageLabel}`)
+                : filename}
+            </Typography>
+          }
         />
       </ListItem>
       <ListItem>
@@ -62,7 +98,11 @@ const Qualification = ({ file, t }) => {
               {t('viewer.panel.qualification.qualification')}
             </Typography>
           }
-          secondary={<Typography variant={'body1'}>In coming</Typography>}
+          secondary={
+            <Typography variant={'body1'}>
+              {scannerT(`Scan.items.${label}`)}
+            </Typography>
+          }
         />
       </ListItem>
     </List>
