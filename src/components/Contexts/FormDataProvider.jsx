78de1fd5e93974cae5f2dcedf98db779f534b0a6
Alexis G <merkkur39@gmail.com>
2021-10-07 16:54:38 +0200

    feat: Added Alerter when Paper is saved

--- src/components/Contexts/FormDataProvider.jsx
+++ src/components/Contexts/FormDataProvider.jsx
@@ -1,13 +1,16 @@
 import React, { createContext, useState } from 'react'
 
 import { models, useClient } from 'cozy-client'
+import { CozyFile } from 'cozy-doctypes'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import Alerter from 'cozy-ui/transpiled/react/Alerter'
 
 import { useStepperDialogContext } from 'src/components/Hooks/useStepperDialogContext'
 import getOrCreateAppFolderWithReference from 'src/utils/getFolderWithReference'
 import { FILES_DOCTYPE } from 'src/doctypes'
 
 const { Qualification } = models.document
+const HTTP_CODE_CONFLICT = 409
 
 const FormDataContext = createContext()
 
@@ -30,6 +33,7 @@ const FormDataProvider = ({ children }) => {
     const { metadata } = formData
 
     ;(async () => {
+      let submitSucceeded = null
       for (const { file, fileMetadata } of formData.data) {
         const newQualification = {
           ...qualification,
@@ -43,12 +47,49 @@ const FormDataProvider = ({ children }) => {
           t
         )
 
-        await client.create(FILES_DOCTYPE, {
-          data: file,
-          metadata: { qualification: newQualification },
-          dirId: appFolderID
-        })
-        setIsStepperDialogOpen(false)
+        try {
+          await client.create(FILES_DOCTYPE, {
+            data: file,
+            metadata: { qualification: newQualification },
+            dirId: appFolderID
+          })
+          if (submitSucceeded === null) submitSucceeded = true
+        } catch (err) {
+          const objError = JSON.parse(JSON.stringify(err))
+          if (objError.status === HTTP_CODE_CONFLICT) {
+            const { filename, extension } = CozyFile.splitFilename({
+              name: file.name,
+              type: 'file'
+            })
+            const newFilename = CozyFile.generateNewFileNameOnConflict(filename)
+            const newFilenameWithExt = `${newFilename}${extension}`
+            const blob = file.slice(0, file.size, file.type)
+            const renamedFile = new File([blob], newFilenameWithExt, {
+              type: file.type
+            })
+
+            try {
+              await client.create(FILES_DOCTYPE, {
+                data: renamedFile,
+                metadata: { qualification: newQualification },
+                dirId: appFolderID
+              })
+              if (submitSucceeded === null) submitSucceeded = true
+            } catch (err) {
+              submitSucceeded = false
+              break
+            }
+          } else {
+            submitSucceeded = false
+            break
+          }
+        }
+      }
+      setIsStepperDialogOpen(false)
+      if (submitSucceeded) {
+        Alerter.success(t('common.saveFile.success'))
+      } else {
+        Alerter.error(t(`common.saveFile.error`))
       }
     })()
   }
