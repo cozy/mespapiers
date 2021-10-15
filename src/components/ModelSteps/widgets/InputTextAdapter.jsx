2fb81e411e8cf1858ce5f43f0a90ed00139efe9b
Alexis G <merkkur39@gmail.com>
2021-10-15 10:03:51 +0200

    feat: Improve InputTextAdapter
    Added function `makeInputTypeAndLength` to manage
    the property `type` & `maxLength` of the input depending on the value
    of the `type` property in the `attributes`
    of the current `acquisitionSteps

--- src/components/ModelSteps/widgets/InputTextAdapter.jsx
+++ src/components/ModelSteps/widgets/InputTextAdapter.jsx
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo, useCallback } from 'react'
+import PropTypes from 'prop-types'
 
 import TextField from 'cozy-ui/transpiled/react/MuiCozyTheme/TextField'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 
+import { makeInputTypeAndLength } from 'src/utils/makeInputTypeAndLength'
+
 const InputTextAdapter = ({ attrs, setValue }) => {
-  const { name, inputLabel, metadata } = attrs
+  const { name, inputLabel, metadata, type } = attrs
   const { t } = useI18n()
   const [value, setState] = useState(metadata[name] || '')
 
@@ -12,16 +15,46 @@ const InputTextAdapter = ({ attrs, setValue }) => {
     setValue(prev => ({ ...prev, [name]: value }))
   }, [name, value, setValue])
 
+  const { inputType, inputMaxLength } = useMemo(
+    () => makeInputTypeAndLength(type),
+    [type]
+  )
+
+  const handleOnChange = useCallback(
+    evt => {
+      const { value } = evt.target
+      if (inputMaxLength > 0 && value.length <= inputMaxLength) {
+        setState(value)
+      } else if (inputMaxLength === 0) {
+        setState(value)
+      }
+    },
+    [inputMaxLength]
+  )
+
   return (
     <TextField
       value={value}
-      onChange={evt => setState(evt.target.value)}
+      type={inputType}
+      onChange={handleOnChange}
       variant={'outlined'}
       label={inputLabel ? t(inputLabel) : ''}
       fullWidth
       autoFocus
     />
   )
+}
+
+const attrsProptypes = PropTypes.shape({
+  name: PropTypes.string,
+  inputLabel: PropTypes.string,
+  metadata: PropTypes.shape({ ['attrsProptypes.name']: PropTypes.string }),
+  type: PropTypes.string
+})
+
+InputTextAdapter.propTypes = {
+  attrs: attrsProptypes.isRequired,
+  setValue: PropTypes.func.isRequired
 }
 
 export default InputTextAdapter
