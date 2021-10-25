309cb670c5d17c14473df6de27af7ea8e3d147b7
Alexis G <merkkur39@gmail.com>
2021-10-25 18:33:22 +0200

    feat: Remove sharing button
    The cozy share button next to the transfer button
    causes too much misunderstanding, until a better
    placement or renaming, we will remove it

--- src/components/Viewer/Footer/BottomSheetContent.jsx
+++ src/components/Viewer/Footer/BottomSheetContent.jsx
@@ -7,7 +7,6 @@ import Paper from 'cozy-ui/transpiled/react/Paper'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import Stack from 'cozy-ui/transpiled/react/Stack'
 
-import Sharing from './Sharing'
 import getPanelBlocks, { panelBlocksSpecs } from '../Panel/getPanelBlocks'
 
 const useStyles = makeStyles(theme => ({
@@ -26,7 +25,6 @@ const BottomSheetContent = forwardRef(({ file, FileActionButton }, ref) => {
       className={cx('u-flex u-flex-column u-ov-hidden', styles.stack)}
     >
       <Paper className={'u-flex u-ph-1 u-pb-1'} elevation={2} square ref={ref}>
-        <Sharing file={file} />
         <FileActionButton file={file} />
       </Paper>
       {panelBlocks.map((PanelBlock, index) => (
