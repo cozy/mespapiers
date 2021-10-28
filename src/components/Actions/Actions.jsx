5b5e7df93b5ece8902fd409a76c0c3328856d93d
Alexis G <merkkur39@gmail.com>
2021-10-29 00:21:46 +0200

    feat: Added "View in Drive" action

--- src/components/Actions/Actions.jsx
+++ src/components/Actions/Actions.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
+import { generateWebLink } from 'cozy-client'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import { ActionMenuItem } from 'cozy-ui/transpiled/react/ActionMenu'
+import Link from 'cozy-ui/transpiled/react/Link'
 import DownloadIcon from 'cozy-ui/transpiled/react/Icons/Download'
 import TrashIcon from 'cozy-ui/transpiled/react/Icons/Trash'
 import ReplyIcon from 'cozy-ui/transpiled/react/Icons/Reply'
+import Folder from 'cozy-ui/transpiled/react/Icons/Folder'
 
 import { isReferencedBy } from 'src/utils/isReferencedBy'
 import { CONTACTS_DOCTYPE } from 'src/doctypes'
@@ -107,6 +110,36 @@ export const trash = ({ pushModal, popModal }) => {
           <Typography variant="body1" color="error">
             {t('action.trash')}
           </Typography>
+        </ActionMenuItem>
+      )
+    }
+  }
+}
+
+export const viewInDrive = ({ client }) => {
+  return {
+    icon: 'folder',
+    Component: function ViewInDrive({ onClick, className, files }) {
+      const { t } = useI18n()
+      const parentFolderId = files[0].relationships.parent.data.id
+
+      const webLink = generateWebLink({
+        slug: 'drive',
+        cozyUrl: client.getStackClient().uri,
+        subDomainType: client.getInstanceOptions().subdomain,
+        pathname: '/',
+        hash: `folder/${parentFolderId}`
+      })
+
+      return (
+        <ActionMenuItem
+          onClick={onClick}
+          className={className}
+          left={<Icon icon={Folder} />}
+        >
+          <Link href={webLink} target="_blank" className={'u-p-0'}>
+            {t('action.viewInDrive')}
+          </Link>
         </ActionMenuItem>
       )
     }
