618f4ef609059f60c94c953740a354a073ba711c
Alexis G <merkkur39@gmail.com>
2021-10-28 08:55:23 +0200

    feat: Convert image to pdf
    When submitting a new Paper, before saving,
    all the chosen images (jpeg & png) are converted into PDF files

--- src/components/Contexts/FormDataProvider.jsx
+++ src/components/Contexts/FormDataProvider.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from 'react'
+import { PDFDocument } from 'pdf-lib'
 
 import { models, useClient } from 'cozy-client'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
@@ -17,6 +18,30 @@ const {
 } = models
 
 const FormDataContext = createContext()
+
+const audioToBase64 = async audioFile => {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader()
+    reader.onerror = reject
+    reader.onload = e => resolve(e.target.result)
+    reader.readAsDataURL(audioFile)
+  })
+}
+// FIX For testing (to deleted)
+const downloadFile = (data, fileName) => {
+  const a = document.createElement('a')
+  a.style.display = 'none'
+  document.body.appendChild(a)
+
+  a.href = window.URL.createObjectURL(
+    new Blob([data], { type: 'application/pdf' })
+  )
+  a.setAttribute('download', fileName)
+  a.click()
+
+  window.URL.revokeObjectURL(a.href)
+  document.body.removeChild(a)
+}
 
 const FormDataProvider = ({ children }) => {
   const client = useClient()
@@ -38,6 +63,7 @@ const FormDataProvider = ({ children }) => {
     const { metadata } = formData
     ;(async () => {
       try {
+        const pdfDoc = await PDFDocument.create()
         // (1/2) For the moment, the current user is the only possible choice
         const user = await fetchCurrentUser(client)
         const { _id: appFolderID } = await getOrCreateAppFolderWithReference(
@@ -49,7 +75,24 @@ const FormDataProvider = ({ children }) => {
           _id: user._id,
           _type: CONTACTS_DOCTYPE
         }
+
         for (const { file, fileMetadata } of formData.data) {
+          if (file.type !== 'application/pdf') {
+            const fileB64 = await audioToBase64(file)
+            let img
+            if (file.type === 'image/png') img = await pdfDoc.embedPng(fileB64)
+            if (file.type === 'image/jpeg') img = await pdfDoc.embedJpg(fileB64)
+            const imgScaled = img.scale(0.5)
+            const page = pdfDoc.addPage()
+            const { width: pageWidth, height: pageHeight } = page.getSize()
+            page.drawImage(img, {
+              x: pageWidth / 2 - imgScaled.width / 2,
+              y: pageHeight / 2 - imgScaled.height / 2,
+              width: imgScaled.width,
+              height: imgScaled.height
+            })
+          }
+
           const newMetadata = {
             qualification: {
               ...qualification,
@@ -86,6 +129,9 @@ const FormDataProvider = ({ children }) => {
           // (2/2) So this user is referenced as a contact on the new papers
           await fileCollection.addReferencedBy(fileCreated, [reference])
         }
+        const pdfBytes = await pdfDoc.save()
+        // FIX For testing (to deleted)
+        downloadFile(pdfBytes, 'pdf-lib_example.pdf')
 
         Alerter.success(t('common.saveFile.success'))
       } catch (error) {
