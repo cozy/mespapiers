a30bfde5bbed0195d9e4546e8c238fc8828ca4dd
Quentin Valmori <quentin.valmori@gmail.com>
2021-10-29 12:07:43 +0200

    refactor: Mount the FixedDialog inside the PlaceholderThemesList

--- src/components/Placeholders/PlaceholdersList.jsx
+++ src/components/Placeholders/PlaceholdersList.jsx
@@ -3,22 +3,19 @@ import PropTypes from 'prop-types'
 import cx from 'classnames'
 
 import ActionMenu from 'cozy-ui/transpiled/react/ActionMenu'
-import { FixedDialog } from 'cozy-ui/transpiled/react/CozyDialogs'
 import Grid from 'cozy-ui/transpiled/react/MuiCozyTheme/Grid'
 import Card from 'cozy-ui/transpiled/react/Card'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import IconStack from 'cozy-ui/transpiled/react/IconStack'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import FileDuotoneIcon from 'cozy-ui/transpiled/react/Icons/FileDuotone'
-import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 
 import ImportDropdown from 'src/components/ImportDropdown/ImportDropdown'
 import { useScannerI18n } from 'src/components/Hooks/useScannerI18n'
 import { useStepperDialog } from 'src/components/Hooks/useStepperDialog'
 import { findPlaceholdersByQualification } from 'src/helpers/findPlaceholders'
 
-const PlaceholdersList = ({ title, onBack, currentQualifItems }) => {
-  const { t } = useI18n()
+const PlaceholdersList = ({ currentQualifItems }) => {
   const [isImportDropdownDisplayed, setIsImportDropdownDisplayed] = useState(
     false
   )
@@ -59,58 +56,49 @@ const PlaceholdersList = ({ title, onBack, currentQualifItems }) => {
   }, [placeholderSelected, showImportDropdown])
 
   return (
-    <FixedDialog
-      onClose={onBack}
-      transitionDuration={0}
-      title={t('PlaceholdersList.title', {
-        name: ` - ${scannerT(`items.${title}`)}`
-      })}
-      open={true}
-      content={
-        <>
-          <Grid container spacing={1}>
-            {allPlaceholders.map((placeholder, idx) => {
-              const stepsExists = placeholder.acquisitionSteps.length > 0
-
-              return (
-                <Grid
-                  key={idx}
-                  item
-                  xs={6}
-                  onClick={() => selectPlaceholder(placeholder, stepsExists)}
-                >
-                  <Card
-                    className={cx('u-flex u-flex-items-center u-ph-half', {
-                      ['u-ov-hidden']: stepsExists,
-                      ['u-o-50']: !stepsExists
-                    })}
-                  >
-                    <IconStack
-                      backgroundIcon={
-                        <Icon
-                          icon={FileDuotoneIcon}
-                          color="var(--primaryColor)"
-                          size={24}
-                        />
-                      }
-                      foregroundIcon={
-                        <Icon
-                          icon={placeholder.icon}
-                          color="var(--primaryColor)"
-                          size={14}
-                        />
-                      }
+    <>
+      <Grid container spacing={1}>
+        {allPlaceholders.map((placeholder, idx) => {
+          const stepsExists = placeholder.acquisitionSteps.length > 0
+          return (
+            <Grid
+              key={idx}
+              item
+              xs={6}
+              onClick={() => selectPlaceholder(placeholder, stepsExists)}
+            >
+              <Card
+                className={cx('u-flex u-flex-items-center u-ph-half', {
+                  ['u-ov-hidden']: stepsExists,
+                  ['u-o-50']: !stepsExists
+                })}
+              >
+                <IconStack
+                  backgroundIcon={
+                    <Icon
+                      icon={FileDuotoneIcon}
+                      color="var(--primaryColor)"
+                      size={24}
                     />
-                    <Typography
-                      variant={'body2'}
-                      className={'u-flex-wrap u-pl-half'}
-                    >
-                      {scannerT(`items.${placeholder.label}`)}
-                    </Typography>
-                  </Card>
-                </Grid>
-              )
-            })}
+                  }
+                  foregroundIcon={
+                    <Icon
+                      icon={placeholder.icon}
+                      color="var(--primaryColor)"
+                      size={14}
+                    />
+                  }
+                />
+                <Typography
+                  variant={'body2'}
+                  className={'u-flex-wrap u-pl-half'}
+                >
+                  {scannerT(`items.${placeholder.label}`)}
+                </Typography>
+              </Card>
+            </Grid>
+          )
+        })}
       </Grid>
       <ActionMenuImportDropdown
         isOpened={shouldDisplayImportDropdown()}
