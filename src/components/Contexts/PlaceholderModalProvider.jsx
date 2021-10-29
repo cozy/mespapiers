8eb42569c412291426efe4a44e5bcf6bde4f6c4e
Alexis G <merkkur39@gmail.com>
2021-10-29 19:26:03 +0200

    feat: Added PlaceholderModalProvider
    Avoid going back to the Fab menu at the end of the creation of a paper

---
+++ src/components/Contexts/PlaceholderModalProvider.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from 'react'
+
+const PlaceholderModalContext = createContext()
+
+const PlaceholderModalProvider = ({ children }) => {
+  const [showPlaceholderThemesList, setShowPlaceholderThemesList] = useState(
+    false
+  )
+
+  return (
+    <PlaceholderModalContext.Provider
+      value={{ showPlaceholderThemesList, setShowPlaceholderThemesList }}
+    >
+      {children}
+    </PlaceholderModalContext.Provider>
+  )
+}
+
+export default PlaceholderModalContext
+
+export { PlaceholderModalProvider }
