2ab1978454900a177933ca2d5430d74b8daf09bd
Yannick Chiron <yannick.chiron@gmail.com>
2021-11-10 10:13:31 +0100

    feat: Protect routes using `Onboarding` and `Onboarded` guards
    On first app access we want the user to be redirected to the
    `/onboarding` route
    
    The condition for displaying this route is based on `onboarded` setting
    in `io.cozy.mespapiers.settings`
    
    Redirection is handled by a Router guard that uses cozy-client and
    cozy-realtime to check `io.cozy.mespapiers.settings`

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 import { hot } from 'react-hot-loader'
 import { Route, Switch, Redirect, HashRouter } from 'react-router-dom'
 
-import { useClient, RealTimeQueries } from 'cozy-client'
+import {
+  isQueryLoading,
+  useClient,
+  useQuery,
+  RealTimeQueries
+} from 'cozy-client'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import { Layout, Main, Content } from 'cozy-ui/transpiled/react/Layout'
 import { Sprite as IconSprite } from 'cozy-ui/transpiled/react/Icon'
@@ -11,6 +16,7 @@ import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 import MuiCozyTheme from 'cozy-ui/transpiled/react/MuiCozyTheme'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import Alerter from 'cozy-ui/transpiled/react/Alerter'
+import Spinner from 'cozy-ui/transpiled/react/Spinner'
 
 import HomeWrapper from 'src/components/HomeWrapper/HomeWrapper'
 import PapersList from 'src/components/Papers/PapersList'
@@ -21,6 +27,34 @@ import { useStepperDialog } from 'src/components/Hooks/useStepperDialog'
 import StepperDialogWrapper from 'src/components/StepperDialogWrapper/StepperDialogWrapper'
 import { FormDataProvider } from 'src/components/Contexts/FormDataProvider'
 
+import { getOnboardingStatus } from 'src/helpers/queries'
+
+const OnboardedGuardedRoute = ({
+  component: Component,
+  reverseGuard = false,
+  settingsData,
+  ...rest
+}) => {
+  const onboarded = settingsData?.[0]?.onboarded
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (reverseGuard === true && onboarded === true) {
+          return <Redirect to="/" />
+        } else if (reverseGuard !== true && onboarded !== true) {
+          return <Redirect to="/onboarding" />
+        } else {
+          return <Component {...props} />
+        }
+      }}
+    />
+  )
+}
+
+const OnboardingGuardedRoute = props =>
+  OnboardedGuardedRoute({ reverseGuard: true, ...props })
+
 export const App = () => {
   const client = useClient()
   const { t } = useI18n()
@@ -28,6 +62,11 @@ export const App = () => {
   const { BarCenter } = cozy.bar
 
   const { isStepperDialogOpen } = useStepperDialog()
+
+  const { data: settingsData, ...settingsQuery } = useQuery(
+    getOnboardingStatus.definition,
+    getOnboardingStatus.options
+  )
 
   return (
     <HashRouter>
@@ -43,31 +82,52 @@ export const App = () => {
         )}
         <Main>
           <Content className="app-content">
-            <>
-              <Switch>
-                <Route exact path="/" component={HomeWrapper} />
-                <Route
-                  exact
-                  path="/files/:fileCategory"
-                  component={PapersList}
-                />
-                <Route
-                  exact
-                  path="/file/:fileId"
-                  component={FileViewerWithQuery}
-                />
-                <Route exact path="/onboarding" component={Onboarding} />
-                <Redirect from="*" to="/" />
-              </Switch>
+            {isQueryLoading(settingsQuery) ? (
+              <Spinner
+                size="xxlarge"
+                className="u-flex u-flex-justify-center u-mt-2 u-h-5"
+              />
+            ) : (
+              <>
+                <Switch>
+                  <OnboardedGuardedRoute
+                    exact
+                    path="/"
+                    component={HomeWrapper}
+                    settingsData={settingsData}
+                  />
+                  <OnboardedGuardedRoute
+                    exact
+                    path="/files/:fileCategory"
+                    component={PapersList}
+                    settingsData={settingsData}
+                  />
+                  <OnboardedGuardedRoute
+                    exact
+                    path="/file/:fileId"
+                    component={FileViewerWithQuery}
+                    settingsData={settingsData}
+                  />
+                  <OnboardingGuardedRoute
+                    exact
+                    path="/onboarding"
+                    component={Onboarding}
+                    settingsData={settingsData}
+                  />
 
-              {isStepperDialogOpen && (
-                <FormDataProvider>
-                  <StepperDialogWrapper />
-                </FormDataProvider>
-              )}
-            </>
+                  <Redirect from="*" to="/" />
+                </Switch>
+
+                {isStepperDialogOpen && (
+                  <FormDataProvider>
+                    <StepperDialogWrapper />
+                  </FormDataProvider>
+                )}
+              </>
+            )}
           </Content>
           <RealTimeQueries doctype="io.cozy.files" />
+          <RealTimeQueries doctype="io.cozy.mespapiers.settings" />
           <Alerter t={t} />
           <ModalStack />
         </Main>
