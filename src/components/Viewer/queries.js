33262fac38a93858fda9cb32a28a63f49d02b6f3
Alexis G <merkkur39@gmail.com>
2021-11-23 17:51:36 +0100

    feat: Update Qualification panel
    Update Qualification panel with the new evolution of contact

--- src/components/Viewer/queries.js
+++ src/components/Viewer/queries.js
@@ -3,14 +3,17 @@ import { Q, fetchPolicies } from 'cozy-client'
 const defaultFetchPolicy = fetchPolicies.olderThan(30 * 1000)
 
 export const buildViewerFileQuery = fileId => ({
-  definition: () =>
-    Q('io.cozy.files')
-      .where({
-        _id: fileId
-      })
-      .indexFields(['_id']),
+  definition: () => Q('io.cozy.files').getById(fileId),
   options: {
-    as: `buildViewerFileQuery:${fileId}`,
+    as: `buildViewerFileQuery/${fileId}`,
+    fetchPolicy: defaultFetchPolicy
+  }
+})
+
+export const getContactByIds = ids => ({
+  definition: () => Q('io.cozy.contacts').getByIds(ids),
+  options: {
+    as: `getContactByIds/${ids.join('')}`,
     fetchPolicy: defaultFetchPolicy
   }
 })
