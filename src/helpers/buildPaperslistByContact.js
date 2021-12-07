4ba4030b22ca70ecabab31c70098a2620d869a21
Alexis G <merkkur39@gmail.com>
2021-12-07 16:54:00 +0100

    feat: Add maxDisplay limit for PapersList
    Added a "See more" button if the number of Papers of a contact
    is greater than the `maxDisplay` parameter (see the configuration file).
    On click, reveals all Papers of the current category of this contact

--- src/helpers/buildPaperslistByContact.js
+++ src/helpers/buildPaperslistByContact.js
@@ -1,25 +1,45 @@
 import { models } from 'cozy-client'
-import { isReferencedById } from 'cozy-client/dist/associations'
+import { isReferencedById } from 'cozy-client'
 import { CONTACTS_DOCTYPE } from 'src/doctypes'
-const { getFullname } = models.contact
+const { getDisplayName } = models.contact
+
+const DEFAULT_MAX_DISPLAY = 3
 
 /**
- * @param {IOCozyFile[]} papersList - An array of IOCozyFile
- * @param {object[]} contactsList - An array of cozy contact object
- * @returns {{ contact: string, papers: IOCozyFile[] }[]}
+ * @typedef {object} BuildPaperslistByContactParam
+ * @property {object[]} papersList - Array of IOCozyFile
+ * @property {object[]} contactsList - Array of cozy contact object
+ * @property {Paper[]} [papersDefinitions=[]] Array of Papers
+ * @property {string} [currentFileCategory=''] - Category of qualification
  */
-export const buildPaperslistByContact = (papersList, contactsList) => {
-  const result =
-    Array.isArray(contactsList) && Array.isArray(papersList)
-      ? contactsList.flatMap(contact => [
-          {
-            contact: getFullname(contact),
-            papers: papersList.filter(paper =>
-              isReferencedById(paper, CONTACTS_DOCTYPE, contact._id)
-            )
-          }
-        ])
-      : []
+
+/**
+ * @param {BuildPaperslistByContactParam} param
+ * @returns {{ contact: string, papers: {maxDisplay: number, list: IOCozyFile[]} }[]}
+ */
+export const buildPaperslistByContact = ({
+  papersList,
+  contactsList,
+  papersDefinitions = [],
+  currentFileCategory = ''
+}) => {
+  let result = []
+  if (Array.isArray(contactsList) && Array.isArray(papersList)) {
+    const currentDef = papersDefinitions.find(
+      paperDef => paperDef.label === currentFileCategory
+    )
+    result = contactsList.flatMap(contact => [
+      {
+        contact: getDisplayName(contact),
+        papers: {
+          maxDisplay: currentDef?.maxDisplay || DEFAULT_MAX_DISPLAY,
+          list: papersList.filter(paper =>
+            isReferencedById(paper, CONTACTS_DOCTYPE, contact._id)
+          )
+        }
+      }
+    ])
+  }
 
   return result.sort((a, b) => (a.contact > b.contact ? 1 : -1))
 }
