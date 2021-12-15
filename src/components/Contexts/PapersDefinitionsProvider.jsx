858a0c5a1be2cb14fa260ec48e8a6c40bfee94e2
Alexis G <merkkur39@gmail.com>
2021-12-15 18:45:47 +0100

    feat: Fix fetching custom papersDefinitions file
    - If custom papersDefinitions file is not found, fallback on local file
    - Add warning message when custom papersDefinitions file is used

--- src/components/Contexts/PapersDefinitionsProvider.jsx
+++ src/components/Contexts/PapersDefinitionsProvider.jsx
@@ -4,47 +4,80 @@ import useFlag from 'cozy-flags/dist/useFlag'
 import log from 'cozy-logger'
 import { useClient } from 'cozy-client'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import Alerter from 'cozy-ui/transpiled/react/Alerter'
 
 import papersJSON from 'src/constants/papersDefinitions.json'
-import { fetchJsonFileByName } from 'src/utils/fetchJsonFileByName'
+import { fetchCustomPaperDefinitions } from 'src/utils/fetchCustomPaperDefinitions'
+import { fetchContentFileToJson } from 'src/utils/fetchContentFileToJson'
 
 const PapersDefinitionsContext = createContext()
-
-const paperConfigFilenameCustom = 'papersDefinitions.json'
 
 const PapersDefinitionsProvider = ({ children }) => {
   const client = useClient()
   const { t } = useI18n()
+  const [customPapersDefinitions, setCustomPapersDefinitions] = useState({
+    isLoaded: false,
+    name: '',
+    path: ''
+  })
+
   const customPapersDefinitionsFlag = useFlag('customPapersDefinitions')
   const [papersDefinitions, setPapersDefinitions] = useState([])
 
   useEffect(() => {
     ;(async () => {
       if (customPapersDefinitionsFlag) {
-        const paperJSONCustom = await fetchJsonFileByName(
-          client,
-          paperConfigFilenameCustom
-        )
-        if (paperJSONCustom) {
+        const {
+          paperConfigFilenameCustom,
+          appFolderPath,
+          file
+        } = await fetchCustomPaperDefinitions(client, t)
+        const data = await fetchContentFileToJson(client, file)
+
+        if (data) {
+          setCustomPapersDefinitions({
+            isLoaded: true,
+            name: paperConfigFilenameCustom,
+            path: appFolderPath
+          })
+          setPapersDefinitions(data.papersDefinitions)
           log('info', 'Custom PapersDefinitions loaded')
-          setPapersDefinitions(paperJSONCustom.papersDefinitions)
         } else {
-          log('error', 'Please check file name & format')
+          // If custom papersDefinitions.json not found, fallback on local file
+          Alerter.error(
+            t(`PapersDefinitionsProvider.customPapersDefinitions.error`, {
+              name: paperConfigFilenameCustom,
+              path: appFolderPath
+            }),
+            {
+              buttonText: 'Ok',
+              buttonAction: dismiss => dismiss(),
+              duration: 20000
+            }
+          )
+          setPapersDefinitions(papersJSON.papersDefinitions)
+          log('info', 'PapersDefinitions of the app loaded')
         }
       } else {
-        log('info', 'PapersDefinitions of the app loaded')
+        // If has no custom papersDefinitions Flag
+        setCustomPapersDefinitions({
+          isLoaded: false,
+          name: '',
+          path: ''
+        })
         setPapersDefinitions(papersJSON.papersDefinitions)
+        log('info', 'PapersDefinitions of the app loaded')
       }
     })()
-
-    return () => {
-      setPapersDefinitions([])
-    }
   }, [client, customPapersDefinitionsFlag, t])
 
   return (
     <PapersDefinitionsContext.Provider
-      value={{ papersDefinitions, setPapersDefinitions }}
+      value={{
+        papersDefinitions,
+        setPapersDefinitions,
+        customPapersDefinitions
+      }}
     >
       {children}
     </PapersDefinitionsContext.Provider>
