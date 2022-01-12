67d751486f1db0f47986d377cd637a22ee59512e
Alexis G <merkkur39@gmail.com>
2022-01-12 14:06:49 +0100

    fix: Move PaperFab
    Move the PaperFab to prevent it from overlapping the Claudy Fab

--- src/components/PapersFab/PapersFab.jsx
+++ src/components/PapersFab/PapersFab.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/styles'
 
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Fab from 'cozy-ui/transpiled/react/Fab'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import PlusIcon from 'cozy-ui/transpiled/react/Icons/Plus'
+import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 
 import PlaceholderThemesList from 'src/components/Placeholders/PlaceholderThemesList'
 import { usePlaceholderModal } from 'src/components/Hooks/usePlaceholderModal'
 
-const styleFab = { position: 'fixed', zIndex: 10 }
+const useStyles = makeStyles(() => ({
+  fab: {
+    position: 'fixed',
+    zIndex: 10,
+    bottom: '1rem',
+    right: isDesktop => (isDesktop ? '6rem' : '1rem')
+  }
+}))
 
 export const PapersFab = () => {
   const { t } = useI18n()
+  const { isDesktop } = useBreakpoints()
+  const classes = useStyles(isDesktop)
 
   const {
     showPlaceholderThemesList,
@@ -23,8 +34,7 @@ export const PapersFab = () => {
       <Fab
         color="primary"
         aria-label={t('Home.Fab.ariaLabel')}
-        style={styleFab}
-        className="u-bottom-m u-right-m"
+        className={classes.fab}
         onClick={() => setShowPlaceholderThemesList(true)}
       >
         <Icon icon={PlusIcon} />
