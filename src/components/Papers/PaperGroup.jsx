788074c954e68e3f06d2ef60da2275da245b3899
Alexis G <merkkur39@gmail.com>
2022-01-12 10:12:07 +0100

    feat: Add FileImageLoader to PaperGroup
    Replace PDF icon by FileImageLoader in PaperGroup list
    The image used is not necessarily that of the first file in the list

--- src/components/Papers/PaperGroup.jsx
+++ src/components/Papers/PaperGroup.jsx
@@ -2,8 +2,9 @@ import React, { Fragment, useCallback, useMemo } from 'react'
 import get from 'lodash/get'
 import { useHistory } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
+import uniqBy from 'lodash/uniqBy'
 
-import { useQuery } from 'cozy-client'
+import { useQuery, useClient } from 'cozy-client'
 import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 import List from 'cozy-ui/transpiled/react/MuiCozyTheme/List'
 import ListSubheader from 'cozy-ui/transpiled/react/MuiCozyTheme/ListSubheader'
@@ -13,8 +14,10 @@ import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import Divider from 'cozy-ui/transpiled/react/MuiCozyTheme/Divider'
-import IconPdf from 'cozy-ui/transpiled/react/Icons/FileTypePdf'
+import FileTypeImage from 'cozy-ui/transpiled/react/Icons/FileTypeImage'
 import Right from 'cozy-ui/transpiled/react/Icons/Right'
+import MuiCardMedia from 'cozy-ui/transpiled/react/CardMedia'
+import { FileImageLoader } from 'cozy-ui/transpiled/react/FileImageLoader'
 
 import { getAllQualificationLabel } from 'src/helpers/queries'
 import { useScannerI18n } from 'src/components/Hooks/useScannerI18n'
@@ -26,6 +29,7 @@ const useStyles = makeStyles({
 
 const PaperGroup = () => {
   const classes = useStyles()
+  const client = useClient()
   const { isMobile } = useBreakpoints()
   const history = useHistory()
   const { t } = useI18n()
@@ -41,17 +45,8 @@ const PaperGroup = () => {
     allQualificationLabel.options
   )
 
-  const categories = allPapers
-    ? [
-        ...new Set(
-          allPapers.map(paper => {
-            const label = get(paper, 'metadata.qualification.label')
-            return label
-          })
-        )
-      ].sort(
-        (a, b) => (scannerT(`items.${a}`) > scannerT(`items.${b}`) && 1) || -1
-      )
+  const allPapersByCategories = allPapers
+    ? uniqBy(allPapers, 'metadata.qualification.label')
     : []
 
   const goPapersList = useCallback(
@@ -63,30 +58,55 @@ const PaperGroup = () => {
     [history]
   )
 
+  const getLinkType = useCallback(paper => {
+    const isImage = paper.class === 'image'
+    const isPdf = paper.class === 'pdf'
+    return isImage ? 'small' : isPdf ? 'icon' : undefined
+  }, [])
+
   return (
     <List>
       <ListSubheader classes={isMobile && classes}>
         {t('PapersList.subheader')}
       </ListSubheader>
       <div className={'u-pv-half'}>
-        {categories.map((category, idx) => (
-          <Fragment key={idx}>
-            <ListItem button onClick={() => goPapersList(category)}>
-              <ListItemIcon>
-                <Icon icon={IconPdf} size={32} />
-              </ListItemIcon>
-              <ListItemText primary={scannerT(`items.${category}`)} />
-              <Icon
-                icon={Right}
-                size={16}
-                color={'var(--secondaryTextColor)'}
-              />
-            </ListItem>
-            {idx !== categories.length - 1 && (
-              <Divider variant="inset" component="li" />
-            )}
-          </Fragment>
-        ))}
+        {allPapersByCategories.map((paper, idx) => {
+          const category = get(paper, 'metadata.qualification.label')
+
+          return (
+            <Fragment key={idx}>
+              <ListItem button onClick={() => goPapersList(category)}>
+                <ListItemIcon>
+                  <FileImageLoader
+                    client={client}
+                    file={paper}
+                    linkType={getLinkType(paper)}
+                    render={src => (
+                      <MuiCardMedia
+                        component={'img'}
+                        width={32}
+                        height={32}
+                        image={src}
+                      />
+                    )}
+                    renderFallback={() => (
+                      <Icon icon={FileTypeImage} size={32} />
+                    )}
+                  />
+                </ListItemIcon>
+                <ListItemText primary={scannerT(`items.${category}`)} />
+                <Icon
+                  icon={Right}
+                  size={16}
+                  color={'var(--secondaryTextColor)'}
+                />
+              </ListItem>
+              {idx !== allPapersByCategories.length - 1 && (
+                <Divider variant="inset" component="li" />
+              )}
+            </Fragment>
+          )
+        })}
       </div>
     </List>
   )
