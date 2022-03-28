dcbd7eae3b5664ca02dc8fafd7bc7183256401f5
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-03-28 14:10:48 +0200

    refactor: Remove AllPlaceholdersChoices
    This component was loaded in a condition that was never triggered.
    It was therefore a relic.
    In fact there are no more cases with "other" suggestions

--- src/components/Placeholders/Placeholder.jsx
+++ src/components/Placeholders/Placeholder.jsx
@@ -1,8 +1,6 @@
 import React, { useCallback, useState, Fragment, useMemo, useRef } from 'react'
 import PropTypes from 'prop-types'
 
-import ActionMenu from 'cozy-ui/transpiled/react/ActionMenu'
-import List from 'cozy-ui/transpiled/react/MuiCozyTheme/List'
 import ListItem from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItem'
 import ListItemIcon from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItemIcon'
 import Divider from 'cozy-ui/transpiled/react/MuiCozyTheme/Divider'
@@ -12,93 +10,25 @@ import Icon from 'cozy-ui/transpiled/react/Icon'
 import InfosBadge from 'cozy-ui/transpiled/react/InfosBadge'
 import FileDuotoneIcon from 'cozy-ui/transpiled/react/Icons/FileDuotone'
 import Plus from 'cozy-ui/transpiled/react/Icons/Plus'
-import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 
 import { useStepperDialog } from 'src/components/Hooks/useStepperDialog'
 import { useScannerI18n } from 'src/components/Hooks/useScannerI18n'
 import { usePapersDefinitions } from 'src/components/Hooks/usePapersDefinitions'
 import { PaperDefinitionsPropTypes } from 'src/constants/PaperDefinitionsPropTypes'
-import { hasNextvalue } from 'src/utils/hasNextvalue'
 import ActionMenuImportDropdown from 'src/components/Placeholders/ActionMenuImportDropdown'
 
-const AllPlaceholdersChoices = ({
-  onClick,
-  isOtherPaper,
-  hideAllPapersChoices
-}) => {
-  const scannerT = useScannerI18n()
-  const { papersDefinitions } = usePapersDefinitions()
-  const hasDivider = useCallback(
-    idx => hasNextvalue(idx, papersDefinitions),
-    [papersDefinitions]
-  )
-
-  return (
-    <ActionMenu onClose={hideAllPapersChoices}>
-      <List>
-        {papersDefinitions.map((placeholder, idx) => (
-          <Fragment key={`${placeholder.label}${idx}`}>
-            {!isOtherPaper(placeholder.label) && (
-              <>
-                <ListItem onClick={onClick}>
-                  <ListItemIcon>
-                    <InfosBadge
-                      badgeContent={
-                        <Icon
-                          icon={Plus}
-                          size={10}
-                          color="var(--primaryTextColor)"
-                        />
-                      }
-                    >
-                      <IconStack
-                        backgroundClassName={'u-o-50'}
-                        backgroundIcon={
-                          <Icon
-                            icon={FileDuotoneIcon}
-                            color="var(--primaryColor)"
-                            size={32}
-                          />
-                        }
-                        foregroundIcon={
-                          <Icon
-                            icon={placeholder.icon}
-                            color="var(--primaryColor)"
-                            size={16}
-                          />
-                        }
-                      />
-                    </InfosBadge>
-                  </ListItemIcon>
-                  <Typography variant="body1" color="textSecondary">
-                    {scannerT(`items.${placeholder.label}`)}
-                  </Typography>
-                </ListItem>
-                {hasDivider(idx) && <Divider variant="inset" component="li" />}
-              </>
-            )}
-          </Fragment>
-        ))}
-      </List>
-    </ActionMenu>
-  )
-}
-
 const Placeholder = ({ placeholder, divider }) => {
-  const { t } = useI18n()
   const scannerT = useScannerI18n()
   const actionBtnRef = useRef()
   const { papersDefinitions } = usePapersDefinitions()
   const [isImportDropdownDisplayed, setIsImportDropdownDisplayed] =
     useState(false)
-  const [isPapersLabelsList, setIsPapersLabelsList] = useState(false)
   const { setCurrentDefinition } = useStepperDialog()
   const formModel = useMemo(() => {
     return papersDefinitions.find(
       paper => paper.label && paper.label === placeholder.label
     )
   }, [papersDefinitions, placeholder.label])
-  const isOtherPaper = useCallback(label => label === 'other', [])
 
   const hideImportDropdown = useCallback(
     () => setIsImportDropdownDisplayed(false),
@@ -113,33 +43,9 @@ const Placeholder = ({ placeholder, divider }) => {
     }
   }, [formModel, setCurrentDefinition])
 
-  const showAllPapersChoices = useCallback(
-    () => setIsPapersLabelsList(true),
-    []
-  )
-  const hideAllPapersChoices = useCallback(
-    () => setIsPapersLabelsList(false),
-    []
-  )
-
-  const handleOnClick = useCallback(() => {
-    if (!isOtherPaper(placeholder.label)) showImportDropdown()
-    else showAllPapersChoices()
-  }, [
-    isOtherPaper,
-    placeholder.label,
-    showAllPapersChoices,
-    showImportDropdown
-  ])
-
-  const itemLabel = useMemo(() => {
-    if (isOtherPaper(placeholder.label)) return t(`items.${placeholder.label}`)
-    return scannerT(`items.${placeholder.label}`)
-  }, [isOtherPaper, placeholder.label, scannerT, t])
-
   return (
     <>
-      <ListItem button onClick={handleOnClick} ref={actionBtnRef}>
+      <ListItem button onClick={showImportDropdown} ref={actionBtnRef}>
         <ListItemIcon>
           <InfosBadge
             badgeContent={
@@ -166,7 +72,7 @@ const Placeholder = ({ placeholder, divider }) => {
           </InfosBadge>
         </ListItemIcon>
         <Typography variant="body1" color="textSecondary">
-          {itemLabel}
+          {scannerT(`items.${placeholder.label}`)}
         </Typography>
       </ListItem>
       {divider && <Divider variant="inset" component="li" />}
@@ -177,13 +83,6 @@ const Placeholder = ({ placeholder, divider }) => {
         onClose={hideImportDropdown}
         anchorElRef={actionBtnRef}
       />
-      {isPapersLabelsList && (
-        <AllPlaceholdersChoices
-          onClick={showImportDropdown}
-          isOtherPaper={isOtherPaper}
-          hideAllPapersChoices={hideAllPapersChoices}
-        />
-      )}
     </>
   )
 }
