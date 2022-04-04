17ee86bf047aad2fff9b32e14fdc413c70556e87
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-04-04 11:06:11 +0200

    feat: Remove all useless code, hell yeah ! :fire:
    and empty the locales

--- src/helpers/fetchCurrentUser.js
+++
@@ -1,8 +0,0 @@
-import { CONTACTS_DOCTYPE } from 'src/doctypes'
-
-export const fetchCurrentUser = async client => {
-  const contactCollection = client.collection(CONTACTS_DOCTYPE)
-  const { data: currentUser } = await contactCollection.findMyself()
-
-  return currentUser[0]
-}
