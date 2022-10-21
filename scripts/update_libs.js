c4d2fcbf45f73e6bf20ad6b5a81d976d09aab887
Merkur39 <merkkur39@gmail.com>
2022-10-21 18:09:18 +0200

    chore: Add GH actions
    These scripts allow to compare the peerDeps of the chosen libs with the deps of the app, to update them if necessary and finally create the PR

---
+++ scripts/update_libs.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-console */
+const {
+  getAppDependencies,
+  getPackagesToUpdate,
+  updatePackages,
+  getLibPackageJSON,
+  createPRDescription
+} = require('./utils')
+
+/**
+ * @param {string} libName
+ * @returns
+ */
+const run = async libName => {
+  try {
+    const { version: libVersion, peerDependencies: libPeerDeps } =
+      await getLibPackageJSON(libName)
+
+    const appDeps = await getAppDependencies()
+    const packagesToUpdate = getPackagesToUpdate({
+      libPeerDeps,
+      libName,
+      libVersion,
+      appDeps
+    })
+    await updatePackages(packagesToUpdate)
+    await createPRDescription(packagesToUpdate)
+
+    return Promise.resolve(packagesToUpdate)
+  } catch (error) {
+    return Promise.reject({ libName, message: error.message })
+  }
+}
+
+/**
+ * @param {Opts} opts
+ */
+const main = async opts => {
+  try {
+    const { libs } = opts
+
+    for (const libName of libs) {
+      await run(libName)
+    }
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+
+/**
+ * @typedef Opts
+ * @property {string[]} libs - Name of libs
+ */
+const opts = {
+  libs: ['cozy-mespapiers-lib']
+}
+
+main(opts)
