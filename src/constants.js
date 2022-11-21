a7417e647a8f9a2eecbd8467425ddea3426d17fe
Merkur39 <merkkur39@gmail.com>
2022-11-21 14:54:31 +0100

    feat: Create constants file
    Move all constants in this file
    Remove all useless constants

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const CONTACTS_DOCTYPE = 'io.cozy.contacts'
+export const FILES_DOCTYPE = 'io.cozy.files'
+export const TRIGGERS_DOCTYPE = 'io.cozy.triggers'
+
+export const APP_SLUG = 'mespapiers'
+export const EXPIRATION_SERVICE_NAME = 'expiration'
+export const DEFAULT_NOTICE_PERIOD_DAYS = 90
+export const PERSONAL_SPORTING_LICENCE_NOTICE_PERIOD_DAYS = 15
