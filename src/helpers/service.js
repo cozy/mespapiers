258c0c91898d7d61d4a973120aadc6c2c907e2a8
Merkur39 <merkkur39@gmail.com>
2022-11-25 10:39:51 +0100

    feat: Add helpers for expiration service

---
+++ src/helpers/service.js
@@ -0,0 +1,167 @@
+import sub from 'date-fns/sub'
+import add from 'date-fns/add'
+
+import log from 'cozy-logger'
+import papersDefinitions from 'cozy-mespapiers-lib/dist/constants/papersDefinitions.json'
+
+import {
+  APP_SLUG,
+  DEFAULT_NOTICE_PERIOD_DAYS,
+  PERSONAL_SPORTING_LICENCE_NOTICE_PERIOD_DAYS,
+  TRIGGERS_DOCTYPE
+} from 'src/constants'
+import {
+  buildAllFilesToNotifyQuery,
+  buildTriggerByIdQuery,
+  buildTriggerByServiceNameQuery
+} from 'src/helpers/queries'
+
+/**
+ * @param {CozyClient} client - Instance of CozyClient
+ * @param {string} serviceName - Name of the service
+ * @returns {Promise<Object>} Normalized trigger
+ */
+export const createTriggerByName = async (client, serviceName) => {
+  log('info', `Create trigger with "${serviceName}" service name`)
+  if (!serviceName) {
+    throw new Error('Invalid service name')
+  }
+
+  const attrs = {
+    _type: TRIGGERS_DOCTYPE,
+    type: '@daily',
+    arguments: 'between 1pm and 3pm',
+    worker: 'service',
+    message: {
+      name: serviceName,
+      slug: APP_SLUG
+    }
+  }
+
+  return client.save(attrs)
+}
+
+/**
+ * @param {CozyClient} client - Instance of CozyClient
+ * @param {string} serviceName - Name of the service
+ * @returns {Promise<Object>} Normalized trigger
+ */
+export const fetchOrCreateTriggerByName = async (client, serviceName) => {
+  log('info', `Fetch trigger with "${serviceName}" service name`)
+
+  const triggerByServiceNameQuery = buildTriggerByServiceNameQuery(serviceName)
+  const { data: triggers } = await client.query(
+    triggerByServiceNameQuery.definition
+  )
+
+  if (!triggers || triggers.length === 0) {
+    log('error', `Can't find trigger with "${serviceName}" service name`)
+    return createTriggerByName(client, serviceName)
+  }
+
+  const triggerByIdQuery = buildTriggerByIdQuery(triggers[0].id)
+  return client.query(triggerByIdQuery.definition)
+}
+
+/**
+ * @param {IOCozyFile} file - An CozyFile
+ * @param {string} dateLabel - Label of date
+ * @returns {Date} Normalize expiration date
+ */
+export const computeNormalizeExpirationDate = (file, dateLabel) => {
+  if (file.metadata[dateLabel]) {
+    if (dateLabel === 'referencedDate') {
+      return add(new Date(file.metadata[dateLabel] ?? file.created_at), {
+        days: 365
+      })
+    }
+    return new Date(file.metadata[dateLabel])
+  }
+
+  return null
+}
+
+/**
+ * @param {IOCozyFile} file - An CozyFile
+ * @param {string} dateLabel - Label of date
+ * @returns {Date} Notice date
+ */
+export const computeNoticeDate = (file, dateLabel) => {
+  let noticeDays
+  if (file.metadata[dateLabel]) {
+    if (dateLabel === 'referencedDate') {
+      noticeDays = PERSONAL_SPORTING_LICENCE_NOTICE_PERIOD_DAYS
+    }
+    if (dateLabel === 'expirationDate') {
+      noticeDays =
+        parseInt(file.metadata.noticePeriod, 10) || DEFAULT_NOTICE_PERIOD_DAYS
+    }
+  }
+  if (!noticeDays) {
+    return null
+  }
+
+  const normalizeExpirationDate = computeNormalizeExpirationDate(
+    file,
+    dateLabel
+  )
+  return normalizeExpirationDate
+    ? sub(normalizeExpirationDate, {
+        days: noticeDays
+      })
+    : null
+}
+
+/**
+ * @param {IOCozyFile} file - An CozyFile
+ * @returns {{ label: string, country?: string, expirationDateAttribute: string }[]} papersToNotify - Rule in the paperDefinitions file
+ */
+const getPaperToNotify = file => {
+  const { papersToNotify } = papersDefinitions.notifications
+
+  return papersToNotify.find(({ label, expirationDateAttribute, country }) => {
+    let validCountry = true
+    if (country && country !== 'fr') {
+      validCountry = file.metadata.country === country
+    }
+
+    return (
+      validCountry &&
+      label === file.metadata.qualification.label &&
+      file.metadata[expirationDateAttribute]
+    )
+  })
+}
+
+/**
+ * @param {IOCozyFile[]} files - List of CozyFile
+ * @returns {IOCozyFile[]} List of CozyFile that must be notified
+ */
+export const getfilesNeedNotified = files => {
+  return files.filter(file => {
+    const paperToNotify = getPaperToNotify(file)
+
+    if (paperToNotify) {
+      const noticeDate = computeNoticeDate(
+        file,
+        paperToNotify.expirationDateAttribute
+      )
+      return noticeDate ? new Date() >= noticeDate : false
+    }
+
+    return false
+  })
+}
+
+/**
+ * @param {CozyClient} client - Instance of CozyClient
+ * @returns {Promise<IOCozyFile[]>} List of CozyFile that must be notified
+ */
+export const fetchAllfilesToNotify = async client => {
+  log('info', `Fetch all files to notify`)
+
+  const filesToNotifyQuery = buildAllFilesToNotifyQuery()
+  const files = await client.queryAll(filesToNotifyQuery.definition)
+
+  return getfilesNeedNotified(files)
+}
---
+++ src/helpers/service.spec.js
@@ -0,0 +1,110 @@
+import MockDate from 'mockdate'
+
+import {
+  computeNormalizeExpirationDate,
+  computeNoticeDate,
+  getfilesNeedNotified
+} from 'src/helpers/service'
+
+jest.mock('cozy-mespapiers-lib/dist/constants/papersDefinitions.json', () => ({
+  notifications: {
+    papersToNotify: [
+      {
+        label: 'national_id_card',
+        country: 'fr',
+        expirationDateAttribute: 'expirationDate'
+      },
+      {
+        label: 'residence_permit',
+        expirationDateAttribute: 'expirationDate'
+      },
+      {
+        label: 'personal_sporting_licence',
+        expirationDateAttribute: 'referencedDate'
+      }
+    ]
+  }
+}))
+
+describe('Service', () => {
+  beforeEach(() => {
+    MockDate.set('2022-11-01T11:35:58.118Z')
+  })
+  afterEach(() => {
+    MockDate.reset()
+  })
+
+  const fakeFile01 = {
+    _id: '01',
+    name: 'national id card',
+    created_at: '2022-09-01T00:00:00.000Z',
+    metadata: {
+      qualification: { label: 'national_id_card' },
+      expirationDate: '2022-09-23T11:35:58.118Z'
+    }
+  }
+  const fakeFile02 = {
+    _id: '02',
+    name: 'personal sporting licence',
+    created_at: '2022-09-01T00:00:00.000Z',
+    metadata: {
+      qualification: { label: 'personal_sporting_licence' },
+      referencedDate: '2022-09-23T11:35:58.118Z'
+    }
+  }
+
+  describe('computeExpirationDate', () => {
+    it('should return expirationDate', () => {
+      const res = computeNormalizeExpirationDate(fakeFile01, 'expirationDate')
+
+      expect(res.toISOString()).toBe('2022-09-23T11:35:58.118Z')
+    })
+    it('should return referencedDate plus 365 days', () => {
+      const res = computeNormalizeExpirationDate(fakeFile02, 'referencedDate')
+
+      expect(res.toISOString()).toBe('2023-09-23T11:35:58.118Z')
+    })
+
+    it('should return "null" if metadata is not found', () => {
+      const res = computeNormalizeExpirationDate(fakeFile02, 'expirationDate')
+
+      expect(res).toBeNull()
+    })
+  })
+
+  describe('computeNoticeDate', () => {
+    it('should return notice date for file with expirationDate metadata', () => {
+      const res = computeNoticeDate(fakeFile01, 'expirationDate')
+
+      expect(res.toISOString()).toBe('2022-06-25T11:35:58.118Z')
+    })
+    it('should return notice date for file with referencedDate metadata', () => {
+      const res = computeNoticeDate(fakeFile02, 'referencedDate')
+
+      expect(res.toISOString()).toBe('2023-09-08T11:35:58.118Z')
+    })
+    it('should return null for file without corresponding metadata', () => {
+      const res = computeNoticeDate(fakeFile02, 'expirationDate')
+
+      expect(res).toBeNull()
+    })
+  })
+
+  describe('getfilesNeedNotified', () => {
+    it('should return only files that need to be notified', () => {
+      const res = getfilesNeedNotified([fakeFile01, fakeFile02])
+
+      expect(res).toEqual([
+        {
+          _id: '01',
+          created_at: '2022-09-01T00:00:00.000Z',
+          metadata: {
+            expirationDate: '2022-09-23T11:35:58.118Z',
+            qualification: { label: 'national_id_card' }
+          },
+          name: 'national id card'
+        }
+      ])
+    })
+  })
+})
