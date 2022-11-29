40da19bb4aae094190141f6238fdefd951ff51eb
PolariTOON <36267812+PolariTOON@users.noreply.github.com>
2022-11-29 17:29:40 +0100

    refactor: Remove the previous implementation of expiration helpers

--- src/helpers/service.spec.js
+++ src/helpers/service.spec.js
@@ -1,30 +1,6 @@
 import MockDate from 'mockdate'
 
-import {
-  computeNormalizeExpirationDate,
-  computeNoticeDate,
-  getfilesNeedNotified
-} from 'src/helpers/service'
-
-jest.mock('cozy-mespapiers-lib/dist/constants/papersDefinitions.json', () => ({
-  notifications: {
-    papersToNotify: [
-      {
-        label: 'national_id_card',
-        country: 'fr',
-        expirationDateAttribute: 'expirationDate'
-      },
-      {
-        label: 'residence_permit',
-        expirationDateAttribute: 'expirationDate'
-      },
-      {
-        label: 'personal_sporting_licence',
-        expirationDateAttribute: 'referencedDate'
-      }
-    ]
-  }
-}))
+import { getfilesNeedNotified } from 'src/helpers/service'
 
 describe('Service', () => {
   beforeEach(() => {
@@ -58,43 +34,6 @@ describe('Service', () => {
     name: 'unknown',
     created_at: '2022-09-01T00:00:00.000Z'
   }
-
-  describe('computeExpirationDate', () => {
-    it('should return expirationDate', () => {
-      const res = computeNormalizeExpirationDate(fakeFile01, 'expirationDate')
-
-      expect(res).toBe('2022-09-23T11:35:58.118Z')
-    })
-    it('should return referencedDate plus 365 days', () => {
-      const res = computeNormalizeExpirationDate(fakeFile02, 'referencedDate')
-
-      expect(res).toBe('2022-09-23T11:35:58.118Z')
-    })
-
-    it('should return "null" if metadata is not found', () => {
-      const res = computeNormalizeExpirationDate(fakeFile02, 'expirationDate')
-
-      expect(res).toBeNull()
-    })
-  })
-
-  describe('computeNoticeDate', () => {
-    it('should return notice date for file with expirationDate metadata', () => {
-      const res = computeNoticeDate(fakeFile01, 'expirationDate')
-
-      expect(res).toBe('2022-06-25T11:35:58.118Z')
-    })
-    it('should return notice date for file with referencedDate metadata', () => {
-      const res = computeNoticeDate(fakeFile02, 'referencedDate')
-
-      expect(res).toBe('2022-09-08T11:35:58.118Z')
-    })
-    it('should return null for file without corresponding metadata', () => {
-      const res = computeNoticeDate(fakeFile02, 'expirationDate')
-
-      expect(res).toBeNull()
-    })
-  })
 
   describe('getfilesNeedNotified', () => {
     it('should return only files that need to be notified', () => {
