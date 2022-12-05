e2a7a46a9b77c483b32b97b5c2ba47430749c0dc
Merkur39 <merkkur39@gmail.com>
2022-12-05 12:25:17 +0100

    feat: Services helpers
    - Better date management.
    - The `getfilesNeedNotified` function now returns
    the normalized expiration and notification date,
    in addition to the file itself.

--- src/helpers/service.spec.js
+++ src/helpers/service.spec.js
@@ -49,7 +49,7 @@ describe('Service', () => {
     created_at: '2022-09-01T00:00:00.000Z',
     metadata: {
       qualification: { label: 'personal_sporting_licence' },
-      referencedDate: '2022-09-23T11:35:58.118Z'
+      referencedDate: '2021-09-23T11:35:58.118Z'
     }
   }
 
@@ -57,12 +57,12 @@ describe('Service', () => {
     it('should return expirationDate', () => {
       const res = computeNormalizeExpirationDate(fakeFile01, 'expirationDate')
 
-      expect(res.toISOString()).toBe('2022-09-23T11:35:58.118Z')
+      expect(res).toBe('2022-09-23T11:35:58.118Z')
     })
     it('should return referencedDate plus 365 days', () => {
       const res = computeNormalizeExpirationDate(fakeFile02, 'referencedDate')
 
-      expect(res.toISOString()).toBe('2023-09-23T11:35:58.118Z')
+      expect(res).toBe('2022-09-23T11:35:58.118Z')
     })
 
     it('should return "null" if metadata is not found', () => {
@@ -76,12 +76,12 @@ describe('Service', () => {
     it('should return notice date for file with expirationDate metadata', () => {
       const res = computeNoticeDate(fakeFile01, 'expirationDate')
 
-      expect(res.toISOString()).toBe('2022-06-25T11:35:58.118Z')
+      expect(res).toBe('2022-06-25T11:35:58.118Z')
     })
     it('should return notice date for file with referencedDate metadata', () => {
       const res = computeNoticeDate(fakeFile02, 'referencedDate')
 
-      expect(res.toISOString()).toBe('2023-09-08T11:35:58.118Z')
+      expect(res).toBe('2022-09-08T11:35:58.118Z')
     })
     it('should return null for file without corresponding metadata', () => {
       const res = computeNoticeDate(fakeFile02, 'expirationDate')
@@ -96,13 +96,30 @@ describe('Service', () => {
 
       expect(res).toEqual([
         {
-          _id: '01',
-          created_at: '2022-09-01T00:00:00.000Z',
-          metadata: {
-            expirationDate: '2022-09-23T11:35:58.118Z',
-            qualification: { label: 'national_id_card' }
-          },
-          name: 'national id card'
+          expirationDate: '2022-09-23T11:35:58.118Z',
+          noticeDate: '2022-06-25T11:35:58.118Z',
+          file: {
+            _id: '01',
+            created_at: '2022-09-01T00:00:00.000Z',
+            metadata: {
+              expirationDate: '2022-09-23T11:35:58.118Z',
+              qualification: { label: 'national_id_card' }
+            },
+            name: 'national id card'
+          }
+        },
+        {
+          expirationDate: '2022-09-23T11:35:58.118Z',
+          noticeDate: '2022-09-08T11:35:58.118Z',
+          file: {
+            _id: '02',
+            name: 'personal sporting licence',
+            created_at: '2022-09-01T00:00:00.000Z',
+            metadata: {
+              qualification: { label: 'personal_sporting_licence' },
+              referencedDate: '2021-09-23T11:35:58.118Z'
+            }
+          }
         }
       ])
     })
