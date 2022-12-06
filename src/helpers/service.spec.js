9269402546eb966083c1aa5d9ca197014a29c10d
Merkur39 <merkkur39@gmail.com>
2022-12-06 15:55:11 +0100

    refactor: Put `getfilesNeedNotified` on top of new `cozy-client` helpers
    With a new test.

--- src/helpers/service.spec.js
+++ src/helpers/service.spec.js
@@ -40,7 +40,8 @@ describe('Service', () => {
     created_at: '2022-09-01T00:00:00.000Z',
     metadata: {
       qualification: { label: 'national_id_card' },
-      expirationDate: '2022-09-23T11:35:58.118Z'
+      expirationDate: '2022-09-23T11:35:58.118Z',
+      noticePeriod: '90'
     }
   }
   const fakeFile02 = {
@@ -51,6 +52,11 @@ describe('Service', () => {
       qualification: { label: 'personal_sporting_licence' },
       referencedDate: '2021-09-23T11:35:58.118Z'
     }
+  }
+  const fakeFile03 = {
+    _id: '03',
+    name: 'unknown',
+    created_at: '2022-09-01T00:00:00.000Z'
   }
 
   describe('computeExpirationDate', () => {
@@ -92,25 +98,24 @@ describe('Service', () => {
 
   describe('getfilesNeedNotified', () => {
     it('should return only files that need to be notified', () => {
-      const res = getfilesNeedNotified([fakeFile01, fakeFile02])
+      const res = getfilesNeedNotified([fakeFile01, fakeFile02, fakeFile03])
 
       expect(res).toEqual([
         {
           expirationDate: '2022-09-23T11:35:58.118Z',
-          noticeDate: '2022-06-25T11:35:58.118Z',
           file: {
             _id: '01',
             created_at: '2022-09-01T00:00:00.000Z',
             metadata: {
+              qualification: { label: 'national_id_card' },
               expirationDate: '2022-09-23T11:35:58.118Z',
-              qualification: { label: 'national_id_card' }
+              noticePeriod: '90'
             },
             name: 'national id card'
           }
         },
         {
           expirationDate: '2022-09-23T11:35:58.118Z',
-          noticeDate: '2022-09-08T11:35:58.118Z',
           file: {
             _id: '02',
             name: 'personal sporting licence',
