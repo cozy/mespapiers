ef2d9ce2ce4b0a1205ca6f483bfd2862a5fbddb9
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-05-27 16:08:30 +0200

    feat: Add support for io.cozy.bills

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,7 @@ export const FILES_DOCTYPE = 'io.cozy.files'
 export const TRIGGERS_DOCTYPE = 'io.cozy.triggers'
 export const APP_SETTINGS_DOCTYPE = 'io.cozy.mespapiers.settings'
 export const JOBS_DOCTYPE = 'io.cozy.jobs'
+export const BILLS_DOCTYPE = 'io.cozy.bills'
 
 export const APP_SLUG = 'mespapiers'
 export const EXPIRATION_SERVICE_NAME = 'expiration'
--- src/doctypes/index.js
+++ src/doctypes/index.js
@@ -1,4 +1,38 @@
-import { CONTACTS_DOCTYPE, FILES_DOCTYPE } from 'src/constants'
+import { CONTACTS_DOCTYPE, FILES_DOCTYPE, BILLS_DOCTYPE } from 'src/constants'
+
+import { QueryDefinition, HasMany } from 'cozy-client'
+
+class HasManyBills extends HasMany {
+  get data() {
+    const refs = this.target.relationships.referenced_by.data
+    return refs
+      ? refs
+          .map(ref => {
+            if (ref.type === BILLS_DOCTYPE) {
+              return this.get(ref.type, ref.id)
+            }
+          })
+          .filter(Boolean)
+      : []
+  }
+
+  static query(doc, client, assoc) {
+    if (
+      !doc.relationships ||
+      !doc.relationships.referenced_by ||
+      !doc.relationships.referenced_by.data
+    ) {
+      return null
+    }
+
+    const included = doc.relationships.referenced_by.data
+    const ids = included
+      .filter(inc => inc.type === assoc.doctype)
+      .map(inc => inc.id)
+
+    return new QueryDefinition({ doctype: assoc.doctype, ids })
+  }
+}
 
 // the documents schema, necessary for CozyClient
 export default {
@@ -10,6 +44,11 @@ export default {
   files: {
     doctype: FILES_DOCTYPE,
     attributes: {},
-    relationships: {}
+    relationships: {
+      bills: {
+        type: HasManyBills,
+        doctype: BILLS_DOCTYPE
+      }
+    }
   }
 }
