c6becc46bd6e95d0e1b25094e8a82c1a217f311e
AlexisG <merkkur39@gmail.com>
2024-06-10 16:11:57 +0200

    fix: Add css-modules webpack configuration
    When merging the cozy-mespapiers-lib into the app,
    this configuration should have been added.

--- app.config.js
+++ app.config.js
@@ -1,8 +1,8 @@
-const merge = require('webpack-merge')
 const VersionPlugin = require('cozy-scripts/plugins/VersionPlugin')
 
 const config = [
   require('cozy-scripts/config/webpack.bundle.default.js'),
+  require('cozy-scripts/config/webpack.config.css-modules'),
   require('./app.services')
 ]
 
@@ -43,4 +43,4 @@ const extraConfig = {
 }
 config.push(extraConfig)
 
-module.exports = [merge.apply(null, config)]
+module.exports = config
