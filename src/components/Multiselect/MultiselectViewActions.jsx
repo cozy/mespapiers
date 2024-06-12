70c79b51639af99722133ba5431b30def9556c8a
AlexisG <merkkur39@gmail.com>
2024-06-12 12:14:52 +0200

    feat: The `useAction` hook directly passes the `navigate` prop

--- src/components/Multiselect/MultiselectViewActions.jsx
+++ src/components/Multiselect/MultiselectViewActions.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 import { useMultiSelection } from 'src/components/Contexts/MultiSelectionProvider'
 import { useActions } from 'src/components/Hooks/useActions'
 import MultiselectBackdrop from 'src/components/Multiselect/MultiselectBackdrop'
@@ -9,11 +8,10 @@ import ActionsBar from 'cozy-ui/transpiled/react/ActionsBar'
 const MultiselectViewActions = () => {
   const { allMultiSelectionFiles } = useMultiSelection()
   const [isBackdropOpen, setIsBackdropOpen] = useState(false)
-  const navigate = useNavigate()
 
   const actions = useActions(allMultiSelectionFiles, {
     isActionBar: true,
-    actionsOptions: { navigate, setIsBackdropOpen }
+    actionsOptions: { setIsBackdropOpen }
   })
 
   return (
