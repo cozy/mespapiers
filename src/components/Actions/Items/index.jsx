bd883bd6321ede135eb67e161208119b9c715ace
AlexisG <merkkur39@gmail.com>
2024-06-12 12:13:21 +0200

    feat: Remove duplicate `forward` action

--- src/components/Actions/Items/index.jsx
+++ src/components/Actions/Items/index.jsx
@@ -1,7 +1,6 @@
 export { createPaper } from './createPaper'
 export { createPaperByQualificationLabel } from './createPaperByQualificationLabel'
 export { download } from './download'
-export { forward } from './forward'
 export { importAuto } from './importAuto'
 export { open } from './open'
 export { rename } from './rename'
