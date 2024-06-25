86d4e63199422bafffbc444ade8dd7b91b900807
AlexisG <merkkur39@gmail.com>
2024-06-25 11:45:47 +0200

    feat(MultiselectViewActions): The backdrop is no longer used

--- src/components/Multiselect/MultiselectViewActions.jsx
+++ src/components/Multiselect/MultiselectViewActions.jsx
@@ -1,27 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useMultiSelection } from 'src/components/Contexts/MultiSelectionProvider'
 import { useActions } from 'src/components/Hooks/useActions'
-import MultiselectBackdrop from 'src/components/Multiselect/MultiselectBackdrop'
 
 import ActionsBar from 'cozy-ui/transpiled/react/ActionsBar'
 
 const MultiselectViewActions = () => {
   const { allMultiSelection } = useMultiSelection()
-  const [isBackdropOpen, setIsBackdropOpen] = useState(false)
   const allMultiSelectionFiles = allMultiSelection.map(({ file }) => file)
 
   const actions = useActions(allMultiSelectionFiles, {
-    isActionBar: true,
-    actionsOptions: { setIsBackdropOpen }
+    isActionBar: true
   })
 
-  return (
-    <>
-      {isBackdropOpen && <MultiselectBackdrop />}
-
-      <ActionsBar actions={actions} docs={allMultiSelectionFiles} />
-    </>
-  )
+  return <ActionsBar actions={actions} docs={allMultiSelectionFiles} />
 }
 
 export default MultiselectViewActions
