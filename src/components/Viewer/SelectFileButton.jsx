c4ecbe351680bd13ede6252358741e487034af23
AlexisG <merkkur39@gmail.com>
2024-06-26 13:49:50 +0200

    feat(Viewer): Update SelectFileButton to handle file selecting
    The handleClick function now calls
    the handleFileSelecting function with the
    necessary parameters to handle file selecting.

--- src/components/Viewer/SelectFileButton.jsx
+++ src/components/Viewer/SelectFileButton.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { handleFileSelecting } from 'src/components/Actions/handleFileSelecting'
+import { normalizeFilesWithPage } from 'src/components/Actions/utils'
+import { useModal } from 'src/components/Contexts/ModalProvider'
 import { useMultiSelection } from 'src/components/Contexts/MultiSelectionProvider'
 
+import { useClient } from 'cozy-client'
 import Button from 'cozy-ui/transpiled/react/Buttons'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import IconButton from 'cozy-ui/transpiled/react/IconButton'
@@ -11,6 +15,8 @@ import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 const SelectFileButton = ({ file }) => {
   const { isDesktop } = useBreakpoints()
   const { t } = useI18n()
+  const client = useClient()
+  const { pushModal, popModal } = useModal()
   const navigate = useNavigate()
   const { addMultiSelectionItems } = useMultiSelection()
 
@@ -19,7 +25,14 @@ const SelectFileButton = ({ file }) => {
 
   const handleClick = () => {
     navigate(`/paper/multiselect`)
-    addMultiSelectionItems([{ file }])
+    handleFileSelecting({
+      client,
+      filesWithPage: normalizeFilesWithPage([file]),
+      addMultiSelectionItems,
+      pushModal,
+      popModal,
+      t
+    })
   }
 
   if (isDesktop) {
