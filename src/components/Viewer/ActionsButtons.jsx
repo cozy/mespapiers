56a4a7a55693f1fcd7da57a16c5a67f608da463d
AlexisG <merkkur39@gmail.com>
2024-06-26 17:57:10 +0200

    feat(Viewer): Update ActionsButtons to handle file forwarding

--- src/components/Viewer/ActionsButtons.jsx
+++ src/components/Viewer/ActionsButtons.jsx
@@ -1,22 +1,33 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { handleFileForwarding } from 'src/components/Actions/handleFileForwarding'
+import { normalizeFilesWithPage } from 'src/components/Actions/utils'
+import { useModal } from 'src/components/Contexts/ModalProvider'
 import { useMultiSelection } from 'src/components/Contexts/MultiSelectionProvider'
 import SelectFileButton from 'src/components/Viewer/SelectFileButton'
 
+import { useClient } from 'cozy-client'
 import { ForwardButton } from 'cozy-ui/transpiled/react/Viewer'
+import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
 const ActionsButtons = ({ file, toolbar }) => {
   const navigate = useNavigate()
+  const { client } = useClient()
+  const { pushModal, popModal } = useModal()
+  const { t } = useI18n()
   const { isMultiSelectionActive } = useMultiSelection()
 
   if (isMultiSelectionActive) return null
 
   const handleForwardClick = () => {
-    const fileId = file._id
-    navigate({
-      pathname: 'forward',
-      search: `?fileIds=${fileId}`
+    handleFileForwarding({
+      client,
+      filesWithPage: normalizeFilesWithPage([file]),
+      pushModal,
+      popModal,
+      navigate,
+      t
     })
   }
 
