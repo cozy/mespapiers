438207afdd7fcb46a75751459284cc7ed4803749
AlexisG <merkkur39@gmail.com>
2024-06-27 15:07:04 +0200

    feat(PagePickerModal): Add PagePickerModal component

---
+++ src/components/PagePickerModal/PagePickerModal.jsx
@@ -0,0 +1,104 @@
+import PropTypes from 'prop-types'
+import React, { Fragment, useState } from 'react'
+import { PagePickerModalContentHeader } from 'src/components/PagePickerModal/PagePickerModalContentHeader'
+import { PagePickerModalItem } from 'src/components/PagePickerModal/PagePickerModalItem'
+
+import Button from 'cozy-ui/transpiled/react/Buttons'
+import { ConfirmDialog } from 'cozy-ui/transpiled/react/CozyDialogs'
+import Divider from 'cozy-ui/transpiled/react/Divider'
+import List from 'cozy-ui/transpiled/react/List'
+import Paper from 'cozy-ui/transpiled/react/Paper'
+
+export const PagePickerModal = ({
+  onClose,
+  file,
+  options,
+  onClick,
+  textAction
+}) => {
+  const [selectedChoice, setSelectedChoice] = useState([])
+  const hasMasterChoice = options.flat().some(option => option.master)
+  const disabledConfirm =
+    hasMasterChoice &&
+    selectedChoice.length === 1 &&
+    selectedChoice.some(selChoice => selChoice.master)
+
+  const handleChange = option => {
+    if (selectedChoice.some(selChoice => selChoice.value === option.value)) {
+      if (option.master) {
+        return setSelectedChoice([])
+      }
+      setSelectedChoice(prevChoices =>
+        prevChoices.filter(choice => option.value !== choice.value)
+      )
+    } else {
+      if (option.master) {
+        return setSelectedChoice(options.flat())
+      }
+      setSelectedChoice(choices => [...choices, option])
+    }
+  }
+
+  const handleConfirm = () => {
+    onClick(selectedChoice)
+    setSelectedChoice([])
+    onClose()
+  }
+
+  return (
+    <ConfirmDialog
+      open
+      onClose={onClose}
+      content={
+        <>
+          <PagePickerModalContentHeader file={file} />
+          <div className="u-stack-m">
+            {options.map((optionGr, indexOptGr) => (
+              <Paper key={indexOptGr}>
+                <List className="u-p-0">
+                  {optionGr.map((option, indexOption) => (
+                    <Fragment key={`'${indexOptGr}${indexOption}`}>
+                      <PagePickerModalItem
+                        option={option}
+                        hasMasterChoice={hasMasterChoice}
+                        selectedChoice={selectedChoice}
+                        onChange={handleChange}
+                      />
+                      {indexOption !== optionGr.length - 1 && (
+                        <Divider component="li" variant="inset" />
+                      )}
+                    </Fragment>
+                  ))}
+                </List>
+              </Paper>
+            ))}
+          </div>
+        </>
+      }
+      actions={
+        <Button
+          label={textAction}
+          onClick={handleConfirm}
+          fullWidth
+          disabled={disabledConfirm}
+        />
+      }
+    />
+  )
+}
+
+PagePickerModal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(
+    PropTypes.arrayOf(
+      PropTypes.shape({
+        label: PropTypes.string.isRequired,
+        value: PropTypes.string.isRequired,
+        master: PropTypes.bool
+      })
+    )
+  ).isRequired,
+  textAction: PropTypes.string.isRequired,
+  file: PropTypes.object.isRequired
+}
---
+++ src/components/PagePickerModal/PagePickerModalItem.jsx
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import Checkbox from 'cozy-ui/transpiled/react/Checkbox'
+import ListItem from 'cozy-ui/transpiled/react/ListItem'
+import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
+import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
+
+export const PagePickerModalItem = ({
+  option,
+  hasMasterChoice,
+  selectedChoice,
+  onChange
+}) => {
+  const { t } = useI18n()
+  const disabledChoiceOption =
+    hasMasterChoice &&
+    !option.master &&
+    !selectedChoice.some(choice => choice.master)
+
+  const handleChange = () => {
+    onChange(option)
+  }
+
+  return (
+    <ListItem
+      button
+      size="large"
+      onClick={handleChange}
+      disabled={disabledChoiceOption}
+    >
+      <ListItemIcon>
+        <Checkbox
+          checked={selectedChoice.some(choice => choice.value === option.value)}
+          name={t(option.label)}
+          value={option.value}
+        />
+      </ListItemIcon>
+      <ListItemText primary={t(option.label)} />
+    </ListItem>
+  )
+}
+
+PagePickerModalItem.propTypes = {
+  hasMasterChoice: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+  selectedChoice: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+      master: PropTypes.bool
+    })
+  ).isRequired,
+  option: PropTypes.shape({
+    label: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    master: PropTypes.bool
+  }).isRequired
+}
