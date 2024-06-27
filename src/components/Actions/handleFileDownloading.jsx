061ca9fafefc1d12262ff5ad5775e7fc82464f6f
AlexisG <merkkur39@gmail.com>
2024-06-27 16:03:29 +0200

    feat(Actions): Add handleFileDownloading function
    This function is responsible for checking whether
    one or more files are double-sided or not,
    and for managing how to download them.

---
+++ src/components/Actions/handleFileDownloading.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { downloadFiles, onPickSelectedPage } from 'src/components/Actions/utils'
+import { PagePickerModal } from 'src/components/PagePickerModal/PagePickerModal'
+import { is2SidedFile } from 'src/helpers/is2SidedFile'
+
+/**
+ * Check if the file has 2 sides, and if so, open a modal to choose which side to download
+ * @param {object} params
+ * @param {import('cozy-client/types/CozyClient').default} params.client - CozyClient instance
+ * @param {{file: import('cozy-client/types/types').IOCozyFile, page: string|null}[]} params.filesWithPage - List of files with their page to forward
+ * @param {Function} params.pushModal - Function to push a modal
+ * @param {Function} params.popModal - Function to pop a modal
+ * @param {Function} params.showAlert - Function to show an alert
+ * @param {Function} params.t - Translation function
+ * @param {boolean} [params.fromMultiSelection] - Whether the action is from a multi-selection
+ */
+export const handleFileDownloading = async ({
+  client,
+  filesWithPage,
+  showAlert,
+  pushModal,
+  popModal,
+  t,
+  fromMultiSelection
+}) => {
+  if (
+    filesWithPage.length === 1 &&
+    filesWithPage[0].page === null &&
+    !fromMultiSelection
+  ) {
+    const file = filesWithPage[0].file
+    const isFileWith2side = await is2SidedFile(client, file)
+    if (isFileWith2side) {
+      pushModal(
+        <PagePickerModal
+          file={file}
+          textAction={t('action.download')}
+          onClick={selectedChoice => {
+            const selected = onPickSelectedPage(selectedChoice, file)
+            downloadFiles({ client, t, filesWithPage: selected, showAlert })
+          }}
+          onClose={popModal}
+        />
+      )
+    } else {
+      downloadFiles({ client, t, filesWithPage, showAlert })
+    }
+    return
+  }
+
+  if (filesWithPage.length > 0) {
+    downloadFiles({ client, t, filesWithPage, showAlert })
+  }
+}
