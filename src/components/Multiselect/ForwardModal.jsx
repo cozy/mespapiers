49446fd7c4313124f6f33a8813e7bf5c451fab1b
AlexisG <merkkur39@gmail.com>
2024-07-11 17:12:21 +0200

    feat(ForwardModal): Asynchronous processing before share validation
    Some browsers do not allow asynchronous calls
    to `navigator` methods.
    We therefore revert to the original behavior of
    preparing the file before the final path validation.

--- src/components/Multiselect/ForwardModal.jsx
+++ src/components/Multiselect/ForwardModal.jsx
@@ -1,6 +1,6 @@
 import addDays from 'date-fns/addDays'
 import PropTypes from 'prop-types'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { forwardFile, makeZipFolder } from 'src/components/Actions/utils'
 import { ForwardModalContent } from 'src/components/Multiselect/ForwardModalContent'
 import { LinearBackdrop } from 'src/components/Multiselect/LinearBackdrop'
@@ -11,10 +11,13 @@ import { makeTTL } from 'src/helpers/makeTTL'
 import { useClient } from 'cozy-client'
 import { getSharingLink } from 'cozy-client/dist/models/sharing'
 import { isMobile } from 'cozy-device-helper'
+import minilog from 'cozy-minilog'
 import Button from 'cozy-ui/transpiled/react/Buttons'
 import { ConfirmDialog } from 'cozy-ui/transpiled/react/CozyDialogs'
 import { useAlert } from 'cozy-ui/transpiled/react/providers/Alert'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
+
+const log = minilog('ForwardModal')
 
 const PASSWORD_MIN_LENGTH = 4
 
@@ -30,10 +33,10 @@ export const ForwardModal = ({
   const [password, setPassword] = useState('')
   const [selectedDate, setSelectedDate] = useState(addDays(new Date(), 30))
   const [isValidDate, setIsValidDate] = useState(true)
-  const [isBusy, setIsBusy] = useState(false)
   const [isValidPassword, setIsValidPassword] = useState(true)
   const [dateToggle, setDateToggle] = useState(true)
   const [passwordToggle, setPasswordToggle] = useState(false)
+  const [forwardedFile, setForwardedFile] = useState()
 
   const isDesktopOrMobileWithoutShareAPI =
     (isMobile() && !navigator.share) || !isMobile()
@@ -48,31 +51,65 @@ export const ForwardModal = ({
 
   const helperTextDate = t('InputDateAdapter.invalidDateMessage')
 
-  const handleClick = async () => {
-    setIsBusy(true)
-    let file = filesWithPage[0].file
-    if (isMultipleFile) {
-      file = await makeZipFolder({ client, currentUser, filesWithPage, t, f })
-    } else {
-      if (filesWithPage[0].page) {
-        const newFile = await createPdfFileByPage({
-          client,
-          t,
-          filesWithSpecificPage: filesWithPage
+  useEffect(() => {
+    const init = async () => {
+      let file = filesWithPage[0].file
+      try {
+        if (isMultipleFile) {
+          file = await makeZipFolder({
+            client,
+            currentUser,
+            filesWithPage,
+            t,
+            f
+          })
+        } else {
+          if (filesWithPage[0].page) {
+            const newFile = await createPdfFileByPage({
+              client,
+              t,
+              filesWithSpecificPage: filesWithPage
+            })
+            file = newFile[0]
+          }
+        }
+        setForwardedFile(file)
+      } catch (error) {
+        log.error(`Error in init function: ${error}`)
+        showAlert({
+          message: t('ForwardModal.error.init'),
+          severity: 'error',
+          variant: 'filled'
         })
-        file = newFile[0]
+        onForward?.()
       }
     }
+    init()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  if (!forwardedFile) {
+    return <LinearBackdrop text={t('Multiselect.backdrop')} />
+  }
+
+  const handleClick = async () => {
     const ttl = makeTTL(dateToggle && selectedDate)
     if (isDesktopOrMobileWithoutShareAPI) {
-      const url = await getSharingLink(client, [file._id], {
+      const url = await getSharingLink(client, [forwardedFile._id], {
         ttl,
         password
       })
       await copyToClipboard(url, { t, showAlert })
       onForward?.()
     } else {
-      await forwardFile({ client, files: [file], t, ttl, password, showAlert })
+      await forwardFile({
+        client,
+        files: [forwardedFile],
+        t,
+        ttl,
+        password,
+        showAlert
+      })
       onForward?.()
     }
   }
@@ -82,39 +119,36 @@ export const ForwardModal = ({
     : t('ForwardModal.action.mobile')
 
   return (
-    <>
-      <ConfirmDialog
-        open
-        onClose={onClose}
-        data-testid="ForwardModal"
-        content={
-          <ForwardModalContent
-            filesWithPage={filesWithPage}
-            currentUser={currentUser}
-            setIsValidPassword={setIsValidPassword}
-            setIsValidDate={setIsValidDate}
-            helperTextDate={helperTextDate}
-            helperTextPassword={helperTextPassword}
-            dateToggle={dateToggle}
-            setDateToggle={setDateToggle}
-            passwordToggle={passwordToggle}
-            setPasswordToggle={setPasswordToggle}
-            selectedDate={selectedDate}
-            setSelectedDate={setSelectedDate}
-            password={password}
-            setPassword={setPassword}
-          />
-        }
-        actions={
-          <Button
-            label={textAction}
-            onClick={handleClick}
-            disabled={!isValidDate || !isValidPassword || isBusy}
-          />
-        }
-      />
-      {isBusy && <LinearBackdrop />}
-    </>
+    <ConfirmDialog
+      open
+      onClose={onClose}
+      data-testid="ForwardModal"
+      content={
+        <ForwardModalContent
+          filesWithPage={filesWithPage}
+          currentUser={currentUser}
+          setIsValidPassword={setIsValidPassword}
+          setIsValidDate={setIsValidDate}
+          helperTextDate={helperTextDate}
+          helperTextPassword={helperTextPassword}
+          dateToggle={dateToggle}
+          setDateToggle={setDateToggle}
+          passwordToggle={passwordToggle}
+          setPasswordToggle={setPasswordToggle}
+          selectedDate={selectedDate}
+          setSelectedDate={setSelectedDate}
+          password={password}
+          setPassword={setPassword}
+        />
+      }
+      actions={
+        <Button
+          label={textAction}
+          onClick={handleClick}
+          disabled={!isValidDate || !isValidPassword}
+        />
+      }
+    />
   )
 }
 
