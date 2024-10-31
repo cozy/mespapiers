df0696b1305a48e9e78c9e612405d042e7c41184
AlexisG <merkkur39@gmail.com>
2024-10-31 10:26:04 +0100

    refactor(ScanResultCard): Split component to process image and pdf files

--- src/components/ModelSteps/ScanResult/ScanResultCard.jsx
+++ src/components/ModelSteps/ScanResult/ScanResultCard.jsx
@@ -1,89 +1,48 @@
 import PropTypes from 'prop-types'
-import React, { useState, forwardRef } from 'react'
+import React, { forwardRef } from 'react'
 import { useFormData } from 'src/components/Contexts/FormDataProvider'
 import { useStepperDialog } from 'src/components/Contexts/StepperDialogProvider'
-import styles from 'src/components/ModelSteps/ScanResult/ScanResultCard.styl'
-import ScanResultCardActions from 'src/components/ModelSteps/ScanResult/ScanResultCardActions'
+import ScanResultCardImage from 'src/components/ModelSteps/ScanResult/ScanResultCardImage'
+import ScanResultCardPDF from 'src/components/ModelSteps/ScanResult/ScanResultCardPDF'
 import {
   getLastFormDataFile,
   isSameFile
 } from 'src/components/ModelSteps/helpers'
-import RotateImage from 'src/components/ModelSteps/widgets/RotateImage'
 
-import Box from 'cozy-ui/transpiled/react/Box'
-import Card from 'cozy-ui/transpiled/react/Card'
-import Icon from 'cozy-ui/transpiled/react/Icon'
-import Typography from 'cozy-ui/transpiled/react/Typography'
+const ScanResultCard = forwardRef((props, ref) => {
+  const { currentFile, setCurrentFile } = props
+  const { setFormData, formData } = useFormData()
+  const { currentStepIndex } = useStepperDialog()
 
-const isImageType = file => file.type.match(/image\/.*/)
+  const handleSelectedFile = () => {
+    const newData = formData.data.filter(
+      data => !isSameFile(currentFile, data.file)
+    )
+    setCurrentFile(
+      getLastFormDataFile({ formData: { data: newData }, currentStepIndex })
+    )
 
-const ScanResultCard = forwardRef(
-  ({ currentFile, setCurrentFile, setRotationImage, rotationImage }, ref) => {
-    const { setFormData, formData } = useFormData()
-    const { currentStepIndex } = useStepperDialog()
-    const [imgWrapperMinHeight, setImgWrapperMinHeight] = useState(0)
-    const [isImageRotating, setIsImageRotating] = useState(false)
+    setFormData(prev => ({
+      ...prev,
+      data: newData
+    }))
+  }
 
-    const handleSelectedFile = () => {
-      const newData = formData.data.filter(
-        data => !isSameFile(currentFile, data.file)
-      )
-      setCurrentFile(
-        getLastFormDataFile({ formData: { data: newData }, currentStepIndex })
-      )
-
-      setFormData(prev => ({
-        ...prev,
-        data: newData
-      }))
-    }
-
-    const handleRotate = () => {
-      setIsImageRotating(true)
-      setRotationImage(prev => prev - 90)
-    }
-
-    const handleImageLoaded = () => {
-      setIsImageRotating(false)
-      // We don't want to recalculate the size on every rotation
-      if (ref.current && imgWrapperMinHeight === 0) {
-        const maxSize = Math.max(
-          ref.current.offsetWidth,
-          ref.current.offsetHeight
-        )
-        setImgWrapperMinHeight(maxSize)
-      }
-    }
-
+  if (currentFile.type === 'application/pdf') {
     return (
-      <Card className="u-ta-center u-p-1 u-flex u-flex-column u-flex-justify-between">
-        <div className={styles['image-container']}>
-          {isImageType(currentFile) ? (
-            <RotateImage
-              image={URL.createObjectURL(currentFile)}
-              onLoaded={handleImageLoaded}
-              rotation={rotationImage}
-              a11n={{ 'aria-hidden': true }}
-              ref={ref}
-            />
-          ) : (
-            <>
-              <Icon icon="file-type-pdf" size={80} aria-hidden="true" />
-              <Typography className="u-mt-half">{currentFile.name}</Typography>
-            </>
-          )}
-        </div>
-        <Box display="flex" gridGap="1rem" marginTop="1rem">
-          <ScanResultCardActions
-            onRotate={handleRotate}
-            onCancel={handleSelectedFile}
-            isImageRotating={isImageRotating}
-          />
-        </Box>
-      </Card>
+      <ScanResultCardPDF {...props} handleSelectedFile={handleSelectedFile} />
     )
   }
-)
+
+  return (
+    <ScanResultCardImage
+      {...props}
+      handleSelectedFile={handleSelectedFile}
+      ref={ref}
+    />
+  )
+})
+
 ScanResultCard.displayName = 'ScanResultCard'
 
 ScanResultCard.propTypes = {
---
+++ src/components/ModelSteps/ScanResult/ScanResultCardPDF.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import styles from 'src/components/ModelSteps/ScanResult/ScanResultCard.styl'
+import ScanResultCardActions from 'src/components/ModelSteps/ScanResult/ScanResultCardActions'
+
+import Box from 'cozy-ui/transpiled/react/Box'
+import Card from 'cozy-ui/transpiled/react/Card'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import Typography from 'cozy-ui/transpiled/react/Typography'
+
+const ScanResultCardPDF = props => {
+  const { currentFile, handleSelectedFile } = props
+
+  return (
+    <Card className="u-ta-center u-p-1 u-flex u-flex-column u-flex-justify-between">
+      <div className={styles['image-container']}>
+        <Icon icon="file-type-pdf" size={80} aria-hidden="true" />
+        <Typography className="u-mt-half">{currentFile.name}</Typography>
+      </div>
+      <Box display="flex" gridGap="1rem" marginTop="1rem">
+        <ScanResultCardActions onCancel={handleSelectedFile} />
+      </Box>
+    </Card>
+  )
+}
+
+ScanResultCardPDF.propTypes = {
+  currentFile: PropTypes.object.isRequired,
+  handleSelectedFile: PropTypes.func.isRequired
+}
+
+export default ScanResultCardPDF
